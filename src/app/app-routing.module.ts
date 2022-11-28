859b4ef67fab699c12c4e54a45d2454f03274355
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2022-11-28 16:56:53 +0100

    Set up routing for page tabs and bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Import Components
+import { AdminComponent } from './components/admin/admin.component';
+import { EditComponent } from './components/edit/edit.component';
+import { MarketplaceComponent } from './components/marketplace/marketplace.component';
+import { PlanComponent } from './components/plan/plan.component';
+import { SearchComponent } from './components/search/search.component';
+import { WorkspacesComponent } from './components/workspaces/workspaces.component';
+
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: 'edit', component: EditComponent },
+  { path: 'marketplace', component: MarketplaceComponent },
+  { path: 'plan', component: PlanComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'workspaces', component: WorkspacesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
