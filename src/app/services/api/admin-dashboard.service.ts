2453baa26c843addf2c80d3302fd7d7b472a4eef
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2022-12-01 09:13:47 +0100

    Implement admin-dashboard.service (previously: AdminDashboardService)

---
+++ src/app/services/api/admin-dashboard.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ConstantsService } from '../constants.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminDashboardService {
+  APIURL: string = this.oConstantsService.getAPIURL();
+  m_bIgnoreWorkspaceApiUrl: boolean = this.oConstantsService.getIgnoreWorkspaceApiUrl();
+
+  constructor(private oHttp: HttpClient, private oConstantsService: ConstantsService) { }
+
+  findUsersByPartialName(sPartialName: string) {
+    return this.oHttp.get(this.APIURL + '/admin/usersByPartialName?partialName=' + sPartialName);
+  };
+
+  findWorkspacesByPartialName(sPartialName: string) {
+    return this.oHttp.get(this.APIURL + '/admin/workspacesByPartialName?partialName=' + sPartialName);
+  };
+
+  findResourcePermissions(sResourceType: string, sResourceId: string, sUserId: string) {
+    let sUrl = this.APIURL + '/admin/resourcePermissions?';
+
+    if (sResourceType) {
+      sUrl += '&resourceType=' + sResourceType;
+    }
+
+    if (sResourceId) {
+      sUrl += '&resourceId=' + sResourceId;
+    }
+
+    if (sUserId) {
+      sUrl += '&userId=' + sUserId;
+    }
+
+    return this.oHttp.get(sUrl);
+  };
+
+
+  // addResourcePermission (sResourceType: string, sResourceId: string, sUserId: string) {
+  //   return this.oHttp.post(this.APIURL + '/admin/resourcePermissions?resourceType=' + sResourceType + "&resourceId=" + sResourceId + "&userId=" + sUserId);
+  // };
+
+  removeResourcePermission(sResourceType: string, sResourceId: string, sUserId: string) {
+    return this.oHttp.delete(this.APIURL + '/admin/resourcePermissions?resourceType=' + sResourceType + "&resourceId=" + sResourceId + "&userId=" + sUserId);
+  };
+
+  getLatestMetricsEntryByNode(sNodeCode: string) {
+    return this.oHttp.get(this.APIURL + '/admin/metrics/latest?nodeCode=' + sNodeCode);
+  };
+}
