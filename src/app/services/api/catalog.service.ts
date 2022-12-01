c8c2619d8efaf6de7f4b109e177aa0ff68a4938c
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2022-12-01 12:11:08 +0100

    Implement catalog.service (previously: CatalogService)

---
+++ src/app/services/api/catalog.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ConstantsService } from '../constants.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatalogService {
+  APIURL: string = this.oConstantsService.getAPIURL();
+
+  constructor(private oConstantsService: ConstantsService, private oHttp: HttpClient) { }
+
+  downloadByName(sFileName: string, sWorkspace: string, sUrl: string) {
+    let urlParams = "?" + "token=" + this.oConstantsService.getSessionId();
+    urlParams = urlParams + "&" + "filename=" + sFileName + "&workspace=" + sWorkspace;
+
+    let sAPIUrl = this.APIURL;
+
+    if (typeof sUrl !== "undefined") {
+      if (sUrl !== null) {
+        if (sUrl !== "") {
+          sAPIUrl = sUrl;
+        }
+      }
+    }
+
+    window.location.href = sAPIUrl + "/catalog/downloadbyname" + urlParams;
+  };
+
+  newDownloadByName(sFileName: string, sWorkspace: string, sUrl: string) {
+    var urlParams = "?" + "token=" + this.oConstantsService.getSessionId();
+    urlParams = urlParams + "&" + "filename=" + sFileName + "&workspace=" + sWorkspace;
+
+    var sAPIUrl = this.APIURL;
+
+    if (typeof sUrl !== "undefined") {
+      if (sUrl !== null) {
+        if (sUrl !== "") {
+          sAPIUrl = sUrl;
+        }
+      }
+    }
+
+    return this.oHttp.get(sAPIUrl + "/catalog/downloadbyname" + urlParams, { responseType: 'arraybuffer' });
+  };
+
+  ingestFile(sSelectedFile: string, sWorkspace: string) {
+    //return this.oHttp.put(this.APIURL + '/catalog/upload/ingest?file=' + sSelectedFile + '&workspace=' + sWorkspace);
+  };
+
+  uploadFTPFile(oFtpTransferFile: object, sWorkspaceId: string) {
+    return this.oHttp.put(this.APIURL + '/catalog/upload/ftp?workspace=' + sWorkspaceId, oFtpTransferFile);
+  };
+}
