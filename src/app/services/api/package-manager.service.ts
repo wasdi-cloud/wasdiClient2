e68245d45858d5b4807ba9414786779cdfcf50dc
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2022-12-01 14:49:44 +0100

    Implement package-manager.service (previously: PackageManagerService)

---
+++ src/app/services/api/package-manager.service.ts
@@ -0,0 +1,80 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Workspace } from 'src/app/shared/workspace.model';
+import { ConstantsService } from '../constants.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PackageManagerService {
+
+  APIURL: string = this.oConstantsService.getAPIURL();
+  m_sResource: string = "/packageManager";
+
+  constructor(private oConstantsService: ConstantsService, private oHttp: HttpClient) { }
+
+  /**
+   * Fetch Package Manager Info
+   */
+  getPackageManagerInfo(sWorkspaceName: string) {
+    return this.oHttp.get(this.APIURL + this.m_sResource + "/managerVersion?name=" + sWorkspaceName)
+  };
+
+  /**
+   * Return list of packages
+   */
+  getPackagesList(sWorkspaceName: string) {
+    return this.oHttp.get(this.APIURL + this.m_sResource + "/listPackages?name=" + sWorkspaceName);
+  };
+
+  /**
+   * Remove a Library
+   */
+  deleteLibrary(sProcessorId: string, sLibraryName: string) {
+    let oWorkspace: Workspace = this.oConstantsService.getActiveWorkspace();
+    let sWorkspaceId = "-";
+
+    if (oWorkspace.workspaceId) {
+      sWorkspaceId = oWorkspace.workspaceId;
+    }
+
+    return this.oHttp.get(this.APIURL + this.m_sResource + "/environmentupdate?processorId=" + sProcessorId + "&workspace=" + sWorkspaceId + "&updateCommand=removePackage/" + sLibraryName + "/");
+  };
+
+  /**
+   *  Add a Package
+   */
+  addLibrary(sProcessorId: string, sLibraryName: string) {
+    let oWorkspace = this.oConstantsService.getActiveWorkspace();
+    let sWorkspaceId = "-";
+    if (oWorkspace.workspaceId) {
+      sWorkspaceId = oWorkspace.workspaceId;
+    }
+
+    let sUrl = this.APIURL + this.m_sResource +
+      "/environmentupdate?processorId=" +
+      sProcessorId +
+      "&workspace=" +
+      sWorkspaceId;
+
+    if (sLibraryName) {
+      sUrl += "&updateCommand=addPackage/" + sLibraryName + "/";
+    }
+
+    return this.oHttp.get(sUrl);
+  };
+  /**
+   *   Update a Package
+   */
+  upgradeLibrary(sProcessorId: string, sLibraryName: string, sLatestVersion: string) {
+    let oWorkspace = this.oConstantsService.getActiveWorkspace();
+    let sWorkspaceId = "-";
+
+    if (oWorkspace.workspaceId) {
+      sWorkspaceId = oWorkspace.workspaceId;
+    }
+
+    return this.oHttp.get(this.APIURL + this.m_sResource + "/environmentupdate?processorId=" + sProcessorId + "&workspace=" + sWorkspaceId + "&updateCommand=upgradePackage/" + sLibraryName + "/" + sLatestVersion + "/"
+    );
+  };
+}
