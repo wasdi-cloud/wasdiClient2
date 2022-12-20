3fbc362bbbb2b94922cca8a52ce520cdf7947a8a
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2022-12-20 16:11:39 +0100

    Add interface for products

---
+++ src/app/shared/models/product.model.ts
@@ -0,0 +1,15 @@
+export interface Product {
+    bandsGroups: {
+        bands:[],
+        nodeName: string 
+    }, 
+    bbox: string, 
+    description: string, 
+    fileName: string, 
+    metadata: any, 
+    metadataFileCreated: string, 
+    metadataFielReference: any, 
+    name: string, 
+    productFriendlyName: string, 
+    style: any
+}
