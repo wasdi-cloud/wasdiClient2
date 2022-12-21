76510d6c5747a863a3b6cc7e555c795f6263cac9
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2022-12-21 15:19:48 +0100

    Disable 'Edit' tab when there is no active workspace

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { ConstantsService } from 'src/app/services/constants.service';
@@ -8,7 +8,9 @@ import { ConstantsService } from 'src/app/services/constants.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  sActiveWorkspaceId: string = "";
   constructor(public oConstantsService: ConstantsService, public oRouter: Router, public translate: TranslateService) {
     //Register translation languages:
     translate.addLangs(['en', 'fr', 'it', 'de', 'vi', 'id', 'ro'])
@@ -16,6 +18,11 @@ export class HeaderComponent {
     translate.setDefaultLang('en')
   }
 
+  
+
+  ngOnInit(): void {
+      this.sActiveWorkspaceId = this.oConstantsService.getActiveWorkspace().workspaceId; 
+  }
   logout() {
     this.oConstantsService.logOut();
     this.oRouter.navigateByUrl("login")
