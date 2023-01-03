8b2ec1ace2bdab7d029f51224bc2cea13e096545
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-01-03 13:07:36 +0100

    App details page layout

--- src/app/components/app-details/app-details/app-details.component.ts
+++ src/app/components/app-details/app-details/app-details.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProcessorService } from 'src/app/services/api/processor.service';
+import { ConstantsService } from 'src/app/services/constants.service';
 
+
+export interface application { 
+  buyed: boolean, 
+  friendlyName: string, 
+  imgLink: string, 
+  isMine: boolean, 
+  price: number, 
+  processorDescription: string, 
+  processorId: string, 
+  processorName: string, 
+  publisher: string, 
+  score: number, 
+  votes: number,
+}
 @Component({
   selector: 'app-app-details',
   templateUrl: './app-details.component.html',
   styleUrls: ['./app-details.component.css']
 })
-export class AppDetailsComponent {
+export class AppDetailsComponent implements OnInit {
+  activeApplicationName: string = this.oConstantsService.getSelectedApplication()
+  activeApplicationInfo: any = {} as application;
 
+  constructor(private oConstantsService: ConstantsService, private oProcessorService: ProcessorService, private oRouter: Router) { }
+
+  ngOnInit(): void {
+    console.log(this.activeApplicationName)
+    this.getApplicationDetails(this.activeApplicationName)
+  }
+
+  getApplicationDetails(applicationName: string) {
+    return this.oProcessorService.getMarketplaceDetail(applicationName).subscribe(response => {
+      this.activeApplicationInfo = response
+      console.log(response)
+    });
+  }
+
+  marketplaceReturn(){
+    this.oRouter.navigateByUrl('marketplace')
+  }
 }
