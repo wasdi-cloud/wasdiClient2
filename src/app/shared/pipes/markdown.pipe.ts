027261a4694d14ff26fb5151138881c7a9d8f2e8
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-01-05 13:44:27 +0100

    Add package 'marked' and implement markdown pipe

--- src/app/shared/pipes/markdown.pipe.ts
+++ src/app/shared/pipes/markdown.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+import { marked } from 'marked';
 @Pipe({
   name: 'markdown'
 })
 export class MarkdownPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, args?: any[]): any {
+    if (value && value.length > 0) {
+      console.log(marked.parse(value))
+      return marked.parse(value);
+    }
+    return value;
   }
 
 }
