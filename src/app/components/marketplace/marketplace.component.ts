5a634c71bc4c4027c0a8077832572971d44d3f37
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-01-10 09:14:31 +0100

    Align naming of oProcessor in marketplace and app card components

--- src/app/components/marketplace/marketplace.component.ts
+++ src/app/components/marketplace/marketplace.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { tap } from 'rxjs';
 import { ProcessorService } from 'src/app/services/api/processor.service';
-import {MatDialog, MatDialogConfig} from "@angular/material/dialog"
- 
+import { MatDialog } from "@angular/material/dialog"
+import { FormControl } from '@angular/forms';
+import { ProcessorMediaService } from 'src/app/services/api/processor-media.service';
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
@@ -24,17 +24,40 @@ export class MarketplaceComponent implements OnInit {
 
   m_aoApplications: any = [];
 
+  category = new FormControl('');
+  m_asCategoryOptions: any = [];
+  publishers: any = [];
 
-  constructor(private oProcessorService: ProcessorService, private dialog: MatDialog) {}
+  publisherFilter!: string;
+  searchWord!: string;
+
+  constructor(private oProcessorService: ProcessorService, private oProcessorMediaService: ProcessorMediaService, private dialog: MatDialog) { }
 
   
   ngOnInit(): void {
-      this.oProcessorService.getMarketplaceList(this.m_oAppFilter).subscribe(response => {
-        console.log(response)
-        this.m_aoApplications = response;
-      })
-  }  
+    this.oProcessorService.getMarketplaceList(this.m_oAppFilter).subscribe(response => {
+      console.log(response)
+      this.m_aoApplications = response;
+    })
+    this.getCategories();
+    this.getPublishers();
+  }
 
+  getCategories() {
+    this.oProcessorMediaService.getCategories().subscribe(response => {
+      this.m_asCategoryOptions = response
+      console.log(response);
+    })
+  }
 
+  getPublishers() {
+    this.oProcessorMediaService.getPublishersFilterList().subscribe(response => {
+      this.publishers = response;
+      console.log(response)
+    })
+  }
 
+  searchApps() {
+    return this.m_aoApplications.filter(oApplication => oApplication.friendlyName.includes(this.searchWord))
+  }
 }
