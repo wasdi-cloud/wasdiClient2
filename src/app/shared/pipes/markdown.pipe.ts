5ef02d1245b4a1c995cdd5c05da16ab92b372c79
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-01-10 10:02:14 +0100

    Remove markdown pipe console.log

--- src/app/shared/pipes/markdown.pipe.ts
+++ src/app/shared/pipes/markdown.pipe.ts
@@ -7,7 +7,6 @@ export class MarkdownPipe implements PipeTransform {
 
   transform(value: any, args?: any[]): any {
     if (value && value.length > 0) {
-      console.log(marked.parse(value))
       return marked.parse(value);
     }
     return value;
