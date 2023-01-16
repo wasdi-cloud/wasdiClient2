6a1ab18fd1fc35d0493302cd0574015ddda97ffe
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-01-16 09:06:23 +0100

    Marketplace filters w/api calls

--- src/app/components/marketplace/marketplace.component.ts
+++ src/app/components/marketplace/marketplace.component.ts
@@ -3,13 +3,29 @@ import { ProcessorService } from 'src/app/services/api/processor.service';
 import { MatDialog } from "@angular/material/dialog"
 import { FormControl } from '@angular/forms';
 import { ProcessorMediaService } from 'src/app/services/api/processor-media.service';
+
+
+interface AppFilter {
+  categories: [];
+  publishers: string[],
+  name: "",
+  score: 0,
+  minPrice: -1,
+  maxPrice: 1000,
+  itemsPerPage: 12,
+  page: 0,
+  orderBy: string,
+  orderDirection: 1
+}
+
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
   styleUrls: ['./marketplace.component.css']
 })
 export class MarketplaceComponent implements OnInit {
-  m_oAppFilter = {
+  publisher: string;
+  m_oAppFilter: AppFilter = {
     categories: [],
     publishers: [],
     name: "",
@@ -33,12 +49,9 @@ export class MarketplaceComponent implements OnInit {
 
   constructor(private oProcessorService: ProcessorService, private oProcessorMediaService: ProcessorMediaService, private dialog: MatDialog) { }
 
-  
+
   ngOnInit(): void {
-    this.oProcessorService.getMarketplaceList(this.m_oAppFilter).subscribe(response => {
-      console.log(response)
-      this.m_aoApplications = response;
-    })
+    this.getApplications();
     this.getCategories();
     this.getPublishers();
   }
@@ -59,5 +72,45 @@ export class MarketplaceComponent implements OnInit {
 
   searchApps() {
     return this.m_aoApplications.filter(oApplication => oApplication.friendlyName.includes(this.searchWord))
+  }
+
+  getApplications() {
+    this.oProcessorService.getMarketplaceList(this.m_oAppFilter).subscribe(oResponse => {
+      this.m_aoApplications = oResponse
+    })
+  }
+
+  developerChanged(event) {
+    if (this.m_oAppFilter.publishers.length !== 0 || event.target.value === 'all') {
+      this.m_oAppFilter.publishers.splice(0, this.m_oAppFilter.publishers.length)
+      console.log(this.m_oAppFilter)
+    }
+    if (event.target.value === 'all') {
+      this.getApplications()
+    } else {
+      this.m_oAppFilter.publishers.push(event.target.value);
+      this.getApplications()
+    }
+
+  }
+  //Success rate will need to be added
+  successChanged() {
+
+  }
+
+  //most used will need to be added
+  usedChanged() {
+
+  }
+
+  maxPriceChanged(event) {
+    this.m_oAppFilter.maxPrice = event.target.value;
+
+    this.getApplications()
+  }
+
+  ratingChanged(event) {
+    this.m_oAppFilter.score = event.target.value;
+    this.getApplications()
   }
 }
