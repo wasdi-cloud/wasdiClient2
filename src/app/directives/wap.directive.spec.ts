20012001f515d40de1e254f13d89cba03ad6382b
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-01-23 13:35:26 +0100

    Create directive for wap components host

---
+++ src/app/directives/wap.directive.spec.ts
@@ -0,0 +1,8 @@
+import { WapDirective } from './wap.directive';
+
+describe('WapDirective', () => {
+  it('should create an instance', () => {
+    const directive = new WapDirective();
+    expect(directive).toBeTruthy();
+  });
+});
