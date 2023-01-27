c4a72ab2306c64380077d2b93159655d534e808d
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-01-27 09:36:44 +0100

    Get selection from wap date picker component

--- src/app/components/WAP-components/wap-date-time-picker/wap-date-time-picker.component.ts
+++ src/app/components/WAP-components/wap-date-time-picker/wap-date-time-picker.component.ts
@@ -1,4 +1,5 @@
-import { Component, Output } from '@angular/core';
+import { throwDialogContentAlreadyAttachedError } from '@angular/cdk/dialog';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-wap-date-time-picker',
@@ -6,9 +7,13 @@ import { Component, Output } from '@angular/core';
   styleUrls: ['./wap-date-time-picker.component.css']
 })
 export class WapDateTimePickerComponent {
- @Output() inputtedDate: any; 
+  @Input() inputtedDate: string; 
+  @Output() inputtedDateChange = new EventEmitter<any>();
 
- getDate(event) {
-  console.log(event.target.value)
- }
+  getDate(event) {
+    console.log(event.value)
+    this.inputtedDate = event.value; 
+
+    this.inputtedDateChange.emit(this.inputtedDate); 
+  }
 }
