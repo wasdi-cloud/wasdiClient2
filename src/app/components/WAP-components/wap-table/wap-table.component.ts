a96ab21a536cd122536bc1950dc16939f422a76b
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-01-31 11:19:33 +0100

    Emit informaton from table component

--- src/app/components/WAP-components/wap-table/wap-table.component.ts
+++ src/app/components/WAP-components/wap-table/wap-table.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-wap-table',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./wap-table.component.css']
 })
 export class WapTableComponent {
+  @Input() oTableInput: any;
+  @Output() oTableInputChange = new EventEmitter<any>();
 
+  m_asUserInputs: any[] = [];
+
+  aoInputs: any[];
+
+  constructor(private oFormBuilder: FormBuilder) { }
+
+
+  ngOnInit() {
+    this.m_asUserInputs = this.oTableInput.aoTableVariables;
+    // console.log(this.m_asUserInputs);
+    this.aoInputs = []
+    for (let index = 0; index < this.oTableInput.aoTableVariables.length; index++) {
+      this.aoInputs.push([])
+      for (let j = 0; j < this.oTableInput.aoTableVariables[1].length; j++) {
+        this.aoInputs[index].push(undefined)
+      }
+    }
+  }
+
+  emitTableInputs() {
+    // console.log(event.target.value);
+    console.log(this.aoInputs);
+    this.oTableInput.aoTableVariables = this.aoInputs; 
+
+    console.log(this.oTableInput.aoTableVariables)
+    this.oTableInputChange.emit(this.oTableInput);
+  }
+
+  getTableInput(event, iParentIndex, iChildIndex) {
+    console.log(this.m_asUserInputs);
+    console.log(event)
+    console.log(event.target.value)
+
+    this.aoInputs[iParentIndex][iChildIndex] = event.target.value;
+  }
 }
