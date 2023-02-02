3858118f5eac060e1ca4686c14a5f8202de876ee
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-02-02 16:01:10 +0100

    Add model for Bands and import to product model

---
+++ src/app/shared/models/band.model.ts
@@ -0,0 +1,9 @@
+export interface Band {
+    geoserverBoundingBox: any, 
+    geoserverUrl: any, 
+    height: number, 
+    layerId: any, 
+    name: string, 
+    published: boolean, 
+    width: number
+}
