9bc0aa332d2bc14867a209329655675498594aa1
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-02-07 08:47:39 +0100

    Add map drawing capabilities to workspaces map component

--- src/app/components/workspaces/workspaces-map/workspaces-map.component.ts
+++ src/app/components/workspaces/workspaces-map/workspaces-map.component.ts
@@ -1,21 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+import { Component } from "@angular/core";
+import { latLng, Map, tileLayer, featureGroup } from "leaflet";
+import 'node_modules/leaflet-draw/dist/leaflet.draw-src.js';
+import * as L from "leaflet";
 
 @Component({
   selector: 'app-workspaces-map',
   templateUrl: './workspaces-map.component.html',
   styleUrls: ['./workspaces-map.component.css']
 })
-export class WorkspacesMapComponent implements OnInit {
-  map:any;
+export class WorkspacesMapComponent {
+  map: Map;
 
-  ngOnInit() {
-    this.map = L.map('map').setView([0, 0], 3);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(this.map);
-    
+  drawnItems: L.FeatureGroup = featureGroup();
 
+  options = {
+    layers: [
+      tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ],
+    zoom: 15,
+    center: latLng(8.524139, 76.936638),
+    edit: {
+      featureGroup: this.drawnItems
+    }
+  };
 
+  drawOptions: any = {
+    position: 'topright',
+    draw: {
+      marker: {
+        icon: L.icon({
+          iconSize: [25, 41],
+          iconAnchor: [13, 41],
+          iconUrl: 'assets/marker-icon.png',
+          shadowUrl: 'assets/marker-shadow.png'
+        })
+      },
+      rectangle: { showArea: false },
+      polygon: false,
+      circlemarker: false
+    }
+  };
+
+  onDrawCreated(e: any) {
+    const { layerType, layer } = e;
+    if (layerType === "polygon") {
+      const polygonCoordinates = layer._latlngs;
+      console.log(polygonCoordinates);
+    }
+    console.log(layerType)
+    this.drawnItems.addLayer(e.layer);
   }
 }
