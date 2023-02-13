e69948716b71ffe6a068241ba422654c7c9a80cf
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-02-13 09:40:45 +0100

    Reusable map structure in workspaces map

--- src/app/components/workspaces/workspaces-map/workspaces-map.component.ts
+++ src/app/components/workspaces/workspaces-map/workspaces-map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { latLng, Map, tileLayer, featureGroup } from "leaflet";
 import 'node_modules/leaflet-draw/dist/leaflet.draw-src.js';
 import * as L from "leaflet";
@@ -10,16 +10,28 @@ import Geocoder from "leaflet-control-geocoder";
   templateUrl: './workspaces-map.component.html',
   styleUrls: ['./workspaces-map.component.css']
 })
-export class WorkspacesMapComponent {
+export class WorkspacesMapComponent implements OnInit {
   map: any;
   searchControl: Geocoder = new Geocoder;
+  mapOptions: any;
+  layersControl: any;
+  drawOptions: any;
+  drawnItems: any;
+
   constructor(public m_oMapService: MapService) { }
 
-  onMapReady(map: L.Map) {
+  ngOnInit(): void {
+    console.log("init map")
+    this.m_oMapService.setDrawnItems();
+    this.mapOptions = this.m_oMapService.m_oOptions;
+    this.layersControl = this.m_oMapService.m_oLayersControl;
+    this.drawOptions = this.m_oMapService.m_oDrawOptions;
+    this.drawnItems = this.m_oMapService.m_oDrawnItems;
+  }
 
+  onMapReady(map: L.Map) {
     this.searchControl.setPosition('bottomleft')
     this.searchControl.addTo(map);
-
   }
 }
 
