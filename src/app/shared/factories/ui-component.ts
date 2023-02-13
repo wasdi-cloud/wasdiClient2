7204b0182330c629e5ced686c8c82ecdac678426
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-02-13 16:36:39 +0100

    Changes to UI components based on hotfix in prod

--- src/app/shared/factories/ui-component.ts
+++ src/app/shared/factories/ui-component.ts
@@ -1,5 +1,7 @@
 export class UIComponent {
     tooltip: string;
+    required: boolean;
+    label: string;
     constructor() {
         this.tooltip = "";
     }
@@ -51,7 +53,7 @@ export class DateTimePicker extends UIComponent {
         if (this.m_sDate) {
             return this.m_sDate;
         } else {
-            return "";
+            return null;
         }
     }
 
@@ -63,7 +65,7 @@ export class DateTimePicker extends UIComponent {
         if (this.m_sDate) {
             return this.m_sDate;
         } else {
-            return "";
+            return null;
         }
     }
 }
@@ -171,19 +173,23 @@ export class NumericBox extends UIComponent {
             let fValue = parseFloat(this.m_sText)
             // if we can't parse the value as a number
             if (isNaN(fValue)) {
-                // asMessages.push(this.label + " - Please check parameters ");
-                return false;
+                if (!this.required) {
+                    if (this.required) {
+                        asMessages.push(this.label + " - Please check parameters ");
+                        return false;
+                    }
+                }
             }
             if (this.m_fMin) {
                 if (fValue < this.m_fMin) {
-                    //asMessages.push(this.label + " - Value must be greater than " + this.m_fMin);
+                    asMessages.push(this.label + " - Value must be greater than " + this.m_fMin);
                     return false;
                 }
             }
 
             if (this.m_fMax) {
                 if (fValue > this.m_fMax) {
-                    //asMessages.push(this.label + " - Value must be smaller than " + this.m_fMax);
+                    asMessages.push(this.label + " - Value must be smaller than " + this.m_fMax);
                     return false;
                 }
             }
@@ -196,7 +202,13 @@ export class NumericBox extends UIComponent {
     }
 
     getValue() {
-        return parseFloat(this.m_sText);
+        let fFloatValue = parseFloat(this.m_sText);
+        if (isNaN(fFloatValue)) {
+            return null;
+        } else {
+            return parseFloat(this.m_sText);
+        }
+
     }
 
     getStringValue() {
@@ -316,10 +328,16 @@ export class SelectArea extends UIComponent {
     */
     getValue() {
         try {
-            return this.oBoundingBox;
+            let sTest = this.getStringValue();
+
+            if (sTest != "") {
+                return this.oBoundingBox;
+            } else {
+                return null;
+            }
         }
         catch (e) {
-            return "";
+            return null;
         }
     }
     /**
