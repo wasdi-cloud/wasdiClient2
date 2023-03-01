70ddfdda06604ce21e6990ea2b33501848641a9b
Betty SPURGEON <betty.spurgeon@wasdi.cloud>
2023-03-01 16:07:21 +0100

    language toggle flag

--- src/app/components/header/language-switch/language-switch.component.ts
+++ src/app/components/header/language-switch/language-switch.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-
+import { faFlag } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-language-switch',
   templateUrl: './language-switch.component.html',
   styleUrls: ['./language-switch.component.css']
 })
 export class LanguageSwitchComponent {
+  faFlag = faFlag
   constructor(public translate: TranslateService) { }
 
   translateLanguageTo(lang: string) {
     this.translate.use(lang)
   }
 
-  
+
 }
