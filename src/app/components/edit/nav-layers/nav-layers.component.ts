306be7090477e5e8a32acbd374c6164fb88bfd23
Betty <betty.spurgeon@wasdi.cloud>
2023-03-16 17:03:50 +0100

    display visible band info and options

--- src/app/components/edit/nav-layers/nav-layers.component.ts
+++ src/app/components/edit/nav-layers/nav-layers.component.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
-import { faX } from '@fortawesome/free-solid-svg-icons';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { faExpand, faList, faX } from '@fortawesome/free-solid-svg-icons';
+import { MapService } from 'src/app/services/map.service';
 
 @Component({
   selector: 'app-nav-layers',
   templateUrl: './nav-layers.component.html',
   styleUrls: ['./nav-layers.component.css']
 })
-export class NavLayersComponent {
+export class NavLayersComponent implements OnChanges {
+  //Font Awesome Icons:
+  faExpand = faExpand;
+  faList = faList;
+  faX = faX;
 
-  faX = faX; 
-  m_bShowContent: boolean = false; 
+  @Input() m_b2DMapModeOn: boolean;
+  @Input() m_aoVisibleBands
+  @Input() m_aoProducts: any[] = [];
 
-  constructor() {
-    
+  m_sActiveTab: string = 'nav';
+  m_iOpacity: string;
+  thumbLabel = true;
+
+  constructor(
+    private m_oMapService: MapService
+  ) { }
+
+  ngOnChanges(): void {
+    if (this.m_aoVisibleBands !== undefined) {
+      this.setActiveTab('layers');
+    }
   }
+  setActiveTab(sTabName: string) {
+    this.m_sActiveTab = sTabName;
+  }
+
+  getOpacity(event) {
+    console.log(event.srcElement.value)
+  }
+
+  //takes oBand
+  removeBandImage() {
+
+  }
+
 }
