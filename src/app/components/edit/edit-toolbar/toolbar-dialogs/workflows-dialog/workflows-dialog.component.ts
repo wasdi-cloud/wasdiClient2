47645bcbcf96aa565980f040d89ef837c1a88383
Betty <betty.spurgeon@wasdi.cloud>
2023-03-28 10:38:45 +0200

    Show single workflows by user

--- src/app/components/edit/edit-toolbar/toolbar-dialogs/workflows-dialog/workflows-dialog.component.ts
+++ src/app/components/edit/edit-toolbar/toolbar-dialogs/workflows-dialog/workflows-dialog.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { faDownload, faEdit, faLaptopCode, faPlus, faX } from '@fortawesome/free-solid-svg-icons';
+import { WorkflowService } from 'src/app/services/api/workflow.service';
+import { ConstantsService } from 'src/app/services/constants.service';
 
 @Component({
   selector: 'app-workflows-dialog',
@@ -9,11 +11,49 @@ import { faDownload, faEdit, faLaptopCode, faPlus, faX } from '@fortawesome/free
 })
 export class WorkflowsDialogComponent {
   faX = faX;
-  faDownload = faDownload; 
-  faEdit = faEdit; 
-  faLaptop = faLaptopCode; 
-  faPlus = faPlus; 
-  constructor(private m_oMatDialogRef: MatDialogRef<WorkflowsDialogComponent>) { }
+  faDownload = faDownload;
+  faEdit = faEdit;
+  faLaptop = faLaptopCode;
+  faPlus = faPlus;
+
+  m_sUserId: string;
+
+  m_sActiveTab: string = 'execute'
+
+  m_aoWorkflows: any;
+  constructor(
+    private m_oConstantsService: ConstantsService,
+    private m_oMatDialogRef: MatDialogRef<WorkflowsDialogComponent>,
+    private m_oWorkflowService: WorkflowService
+  ) {
+    this.getWorkflows();
+    this.m_sUserId = this.m_oConstantsService.getUserId();
+  }
+
+  setActiveTab(sTabName: string) {
+    this.m_sActiveTab = sTabName;
+  }
+
+  isWorkflowOwner(oWorkflow) {
+    if(!oWorkflow || !this.m_sUserId) {
+      
+      return false;
+    }
+
+    if(this.m_sUserId === oWorkflow.userId) {
+      console.log(oWorkflow)
+      return true; 
+    }
+
+    return false; 
+  }
+
+  getWorkflows() {
+    this.m_oWorkflowService.getWorkflowsByUser().subscribe(oResponse => {
+      console.log(oResponse)
+      this.m_aoWorkflows = oResponse
+    })
+  }
 
   onDismiss() {
     this.m_oMatDialogRef.close()
