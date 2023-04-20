b7ff10f156d2a9fecfa27ec6dd2a21338acd001a
Betty <betty.spurgeon@wasdi.cloud>
2023-04-20 16:04:57 +0200

    Start template removal function in controller

--- src/app/components/edit/edit-toolbar/toolbar-dialogs/apps-dialog/params-library-dialog/params-library-dialog.component.ts
+++ src/app/components/edit/edit-toolbar/toolbar-dialogs/apps-dialog/params-library-dialog/params-library-dialog.component.ts
@@ -3,6 +3,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { faBook, faPlus, faShareNodes, faUpload, faX } from '@fortawesome/free-solid-svg-icons';
 import { ProcessorParamsTemplateService } from 'src/app/services/api/processor-params-template.service';
 import { ConstantsService } from 'src/app/services/constants.service';
+import { ConfirmationDialogModel } from 'src/app/shared/dialogs/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-params-library-dialog',
@@ -35,10 +36,10 @@ export class ParamsLibraryDialogComponent {
     private m_oProcessorParametersTemplateService: ProcessorParamsTemplateService,
   ) {
     this.m_oSelectedProcessor = data;
-    this.m_sProcessorId = this.m_oSelectedProcessor.processorId; 
+    this.m_sProcessorId = this.m_oSelectedProcessor.processorId;
     this.m_sActiveUserId = this.m_oConstantsService.getUserId();
     console.log(this.m_oSelectedProcessor);
-    this.getProcessorParametersTemplateList(this.m_sProcessorId); 
+    this.getProcessorParametersTemplateList(this.m_sProcessorId);
   }
 
   /**
@@ -47,13 +48,13 @@ export class ParamsLibraryDialogComponent {
      * @returns {boolean}
      */
   getProcessorParametersTemplateList(sProcessorId: string) {
-    if(!sProcessorId) {
+    if (!sProcessorId) {
       return false;
     }
     this.m_oProcessorParametersTemplateService.getProcessorParametersTemplatesListByProcessor(sProcessorId).subscribe(oResponse => {
-      console.log(oResponse); 
-      if(oResponse) {
-        this.m_aoParamsTemplates = oResponse; 
+      console.log(oResponse);
+      if (oResponse) {
+        this.m_aoParamsTemplates = oResponse;
       }
     })
     return true;
@@ -68,7 +69,22 @@ export class ParamsLibraryDialogComponent {
   }
 
   deleteProcessorParams(oTemplate: any) {
+    if (!oTemplate) {
+      return false;
+    }
 
+    let sConfirmOwner = `Are you sure you want to delete ${oTemplate.name}?`;
+    let sConfirmShared = `Are you sure you want to remove your permissions from ${oTemplate.name}`;
+
+    let oDialogData = ConfirmationDialogModel;
+
+    if (oTemplate.sharedWithMe) {
+      oTemplate = new ConfirmationDialogModel("Confirm Removal", sConfirmShared)
+    } else {
+      oTemplate = new ConfirmationDialogModel("Confirm Removal", sConfirmOwner)
+    }
+
+    return true;
   }
 
   saveTemplate() {
