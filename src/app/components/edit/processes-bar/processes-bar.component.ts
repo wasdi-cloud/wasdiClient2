1bc6ac756835a80bfe96e621ea6b72c9770a7ef4
Betty <betty.spurgeon@wasdi.cloud>
2023-04-25 13:42:14 +0200

    Fix duration on processes bar

--- src/app/components/edit/processes-bar/processes-bar.component.ts
+++ src/app/components/edit/processes-bar/processes-bar.component.ts
@@ -144,6 +144,73 @@ export class ProcessesBarContent {
     return oFile;
   }
 
+  getDuration(oProcess: any) {
+    if (!oProcess.operationStartDate.endsWith("Z")) {
+      oProcess.operationStartDate += "Z";
+    }
+
+    // start time by server
+    let oStartTime: any = new Date(oProcess.operationStartDate);
+    // still running -> assign "now"
+    let oEndTime: any = new Date();
+    // reassign in case the process is already ended
+    if (oProcess.operationEndDate) {
+
+      if (!oProcess.operationEndDate.endsWith("Z")) {
+        oProcess.operationEndDate += "Z";
+      }
+
+      oEndTime = new Date(oProcess.operationEndDate);
+    }
+
+    if (!oEndTime.getTime()) {
+      oEndTime = new Date(oProcess.lastChangeDate);
+    }
+
+    if (!oEndTime.getTime()) {
+      oEndTime = new Date();
+    }
+
+    //pick time
+    let iMilliseconds = Math.abs(oEndTime - oStartTime);
+
+    //approximate result
+    let iSecondsTimeSpan = Math.ceil(iMilliseconds / 1000);
+
+    if (!iSecondsTimeSpan || iSecondsTimeSpan < 0) {
+      iSecondsTimeSpan = 0;
+    }
+
+    // Calculate number of hours
+    let iHours = Math.trunc(iSecondsTimeSpan / (3600));
+
+    let iMinutesReminder = iSecondsTimeSpan - (iHours * 3600);
+    let iMinutes = Math.trunc(iMinutesReminder / 60);
+    let iSeconds = iMinutesReminder - (iMinutes * 60);
+
+    let sTimeSpan = this.renderTwoDigitNumber(iHours) + ":" + this.renderTwoDigitNumber(iMinutes) + ":" + this.renderTwoDigitNumber(iSeconds);
+
+
+    // var oDate = new Date(1970, 0, 1);
+    // oDate.setSeconds(0 + iSecondsTimeSpan);
+
+    // return oDate;
+    return sTimeSpan;
+  }
+  
+  renderTwoDigitNumber(iNumber: number) {
+    let sNumber = "00";
+
+    if (iNumber > 0) {
+      if (iNumber < 10) {
+        sNumber = "0" + String(iNumber);
+      } else {
+        sNumber = String(iNumber);
+      }
+    }
+
+    return sNumber;
+  }
   openProcessesModal(event: MouseEvent): void {
     const oDialogRef = this.m_oDialog.open(ProcessesDialog, {
       height: '80vh',
