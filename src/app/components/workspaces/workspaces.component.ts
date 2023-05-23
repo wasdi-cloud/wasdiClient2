7f95e228273b70d03cc74debe03bea07b9d8974a
Betty <betty.spurgeon@wasdi.cloud>
2023-05-23 10:18:12 +0200

    show satellites on globe

--- src/app/components/workspaces/workspaces.component.ts
+++ src/app/components/workspaces/workspaces.component.ts
@@ -8,6 +8,8 @@ import { Workspace } from 'src/app/shared/models/workspace.model';
 import { NewWorkspaceDialogComponent } from './new-workspace-dialog/new-workspace-dialog.component';
 import { CesiumService } from 'src/app/shared/cesium.service';
 import { GlobeService } from 'src/app/services/globe.service';
+import { OpportunitySearchService } from 'src/app/services/api/opportunity-search.service';
+import Utils from 'src/app/lib/utils/WasdiJSUtils';
 export interface WorkspaceViewModel {
   activeNode: boolean;
   apiUrl: string;
@@ -34,21 +36,32 @@ export class WorkspacesComponent implements OnInit {
   faPlay = faPlay;
   faStop = faStop;
 
-  constructor(private m_oGlobeService: GlobeService, private oConstantsService: ConstantsService, private m_oDialog: MatDialog, private oWorkspaceService: WorkspaceService) { }
   workspaces: Workspace[] = []
   activeWorkspace!: WorkspaceViewModel;
   sharedUsers!: string[];
+  m_aoSatelliateInputTracks: any[];
+  m_aoSatellitePositions: any[];
+  m_oFakePosition: any = null;
+  m_oUfoPointer: any;
+
+  constructor(
+    private m_oConstantsService: ConstantsService,
+    private m_oDialog: MatDialog,
+    private m_oGlobeService: GlobeService,
+    private m_oOpportunitySearchService: OpportunitySearchService,
+    private oWorkspaceService: WorkspaceService) { }
+
 
   ngOnInit(): void {
     this.fetchWorkspaceInfoList();
     this.m_oGlobeService.initRotateGlobe('CesiumContainer3');
-
+    this.getTrackSatellite();
   }
 
   fetchWorkspaceInfoList() {
     console.log("fetching workspaces")
 
-    let oUser: User = this.oConstantsService.getUser();
+    let oUser: User = this.m_oConstantsService.getUser();
     if (oUser !== {} as User) {
       this.oWorkspaceService.getWorkspacesInfoListByUser().subscribe(response => {
         this.workspaces = response;
@@ -78,6 +91,65 @@ export class WorkspacesComponent implements OnInit {
   }
 
   startGlobeRotation() {
-    this.m_oGlobeService.startRotationGlobe(3); 
+    this.m_oGlobeService.startRotationGlobe(3);
+  }
+
+  getTrackSatellite() {
+    let iSat;
+
+    this.m_aoSatelliateInputTracks = this.m_oGlobeService.getSatelliteTrackInputList();
+
+    //Remove all old Entities from the map:
+    this.m_oGlobeService.removeAllEntities();
+
+    for (let iSat = 0; iSat < this.m_aoSatelliateInputTracks.length; iSat++) {
+      let oActualSat = this.m_aoSatelliateInputTracks[iSat];
+
+      this.m_oOpportunitySearchService.getTrackSatellite(this.m_aoSatelliateInputTracks[iSat].name).subscribe(oResponse => {
+        if (oResponse) {
+          console.log(oResponse)
+          for (let iOriginalSat = 0; iOriginalSat < this.m_aoSatelliateInputTracks.length; iOriginalSat++) {
+            if (this.m_aoSatelliateInputTracks[iOriginalSat].name === oResponse.code) {
+              oActualSat = this.m_aoSatelliateInputTracks[iOriginalSat];
+              break;
+            }
+          }
+
+          let sDescription = oActualSat.description;
+          sDescription += "\n";
+          sDescription += oResponse.currentTime;
+
+          let oActualPosition = this.m_oGlobeService.drawPointWithImage(Utils.utilsProjectConvertCurrentPositionFromServerInCesiumDegrees(oResponse.currentPosition), oActualSat.icon, sDescription, oActualSat.label, 32, 32);
+          this.m_aoSatellitePositions.push(oActualPosition);
+
+          if (this.m_oFakePosition === null) {
+            if (oResponse.lastPositions != null) {
+
+              var iFakeIndex = Math.floor(Math.random() * (oResponse.lastPositions.length));
+
+              this.m_oFakePosition = oResponse.lastPositions[iFakeIndex];
+
+              var aoUfoPosition = Utils.utilsProjectConvertCurrentPositionFromServerInCesiumDegrees(this.m_oFakePosition);
+              aoUfoPosition[2] = aoUfoPosition[2] * 4;
+              this.m_oUfoPointer = this.m_oGlobeService.drawPointWithImage(aoUfoPosition, "assets/icons/alien.svg", "U.F.O.", "?");
+
+              iFakeIndex = Math.floor(Math.random() * (oResponse.lastPositions.length));
+              var aoMoonPosition = Utils.utilsProjectConvertCurrentPositionFromServerInCesiumDegrees(oResponse.lastPositions[iFakeIndex]);
+              //aoMoonPosition [0] = 0.0;
+              //aoMoonPosition[1] = 0.0;
+              aoMoonPosition[2] = 384400000;
+              //aoMoonPosition[2] = 3844000;
+
+              this.m_oGlobeService.drawPointWithImage(aoMoonPosition, "assets/icons/sat_death.svg", "Moon", "-");
+
+            }
+          }
+        }
+      })
+    }
+  }
+
+  updateSatellitesPositions() {
+    
   }
 }
