ebabfe33dd2fd95d2de242c3e24007e8c9894e2b
Betty <betty.spurgeon@wasdi.cloud>
2023-07-14 13:51:51 +0200

    Fix import dialog and add notifications

--- src/app/components/edit/edit-toolbar/toolbar-dialogs/import-dialog/import-dialog.component.ts
+++ src/app/components/edit/edit-toolbar/toolbar-dialogs/import-dialog/import-dialog.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+
+//Angular Material Import: 
 import { MatDialogRef } from '@angular/material/dialog';
-import { faUpload, faX } from '@fortawesome/free-solid-svg-icons';
+
+//Service Imports:
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { CatalogService } from 'src/app/services/api/catalog.service';
+import { ConstantsService } from 'src/app/services/constants.service';
+import { NotificationDisplayService } from 'src/app/services/notification-display.service';
 import { ProductService } from 'src/app/services/api/product.service';
 import { StyleService } from 'src/app/services/api/style.service';
-import { AuthService } from 'src/app/services/auth/auth.service';
-import { ConstantsService } from 'src/app/services/constants.service';
+
+//Font Awesome Import: 
+import { faUpload, faX } from '@fortawesome/free-solid-svg-icons';
+
+//Model Imports
 import { User } from 'src/app/shared/models/user.model';
+
+//Utilities Import: 
+import FadeoutUtils from 'src/app/lib/utils/FadeoutJSUtils';
 
 @Component({
   selector: 'app-import-dialog',
@@ -34,6 +46,7 @@ export class ImportDialogComponent {
     private m_oCatalogService: CatalogService,
     private m_oConstantsService: ConstantsService,
     private m_oDialogRef: MatDialogRef<ImportDialogComponent>,
+    private m_oNotificationDisplayService: NotificationDisplayService,
     private m_oProductService: ProductService,
     private m_oStyleService: StyleService) {
 
@@ -58,34 +71,55 @@ export class ImportDialogComponent {
 
   /*************** UPLOAD ***************/
   onFileSelect(input: any) {
-
     if (input.files && input.files[0]) {
       this.m_sFileName = input.files[0].name
       this.m_oFile = new FormData();
       this.m_oFile.append('file', input.files[0])
-      console.log(this.m_oFile)
     }
   }
 
   onUploadFile() {
-    console.log(this.m_oFile)
+
+    this.m_bIsLoading = true;
+    let sStyle = "";
+
     //Add paywalling in this area on subscriptions
 
-    if (!this.m_sWorkspaceId) {
+    //Check for active workspace:
+    if (FadeoutUtils.utilsIsObjectNullOrUndefined(this.m_sWorkspaceId)) {
+
+    }
+
+    //Check for uploaded file:
+    if (FadeoutUtils.utilsIsObjectNullOrUndefined(this.m_oFile)) {
+      console.log("Please upload a file");
       return false;
     }
 
-    this.m_bIsLoading = true;
-
-    let sStyle = "";
-
-    if (this.m_oStyle) {
+    //If the Style Input is filled apply the style: 
+    if (FadeoutUtils.utilsIsObjectNullOrUndefined(this.m_oStyle) === false) {
       sStyle = this.m_oStyle;
     }
 
-    console.log(this.m_sFileName)
-    this.m_oProductService.uploadFile(this.m_sWorkspaceId, this.m_oFile, this.m_sFileName, sStyle).subscribe(oResponse => {
-      console.log(oResponse);
+    var oBody = new FormData();
+    oBody.append('file', this.m_oFile[0]);
+
+    this.m_oProductService.uploadFile(this.m_sWorkspaceId, oBody, this.m_sFileName, sStyle).subscribe(oResponse => {
+      if (oResponse.status !== 200) {
+        let sErrorMsg: string = "GURU MEDITATION<br>ERROR IN UPLOADING FILE"
+        this.m_oNotificationDisplayService.openSnackBar(sErrorMsg, "Close", "right", "bottom");
+      } else {
+        let sMessage: string = "FILE UPLOADED";
+        this.m_oNotificationDisplayService.openSnackBar(sMessage, "Close", "right", "bottom");
+        this.onDismiss();
+      }
+      this.cleanDragAndDrop();
+      this.m_bIsUploading = false;
+    }, oError => {
+      let sErrorMessage: string = "ERROR IN UPLOADING FILE";
+      this.m_bIsUploading = false;
+      this.cleanDragAndDrop();
+      this.m_oNotificationDisplayService.openSnackBar(sErrorMessage, "Close", "right", "bottom");
     })
     return true
   }
@@ -121,6 +155,10 @@ export class ImportDialogComponent {
 
   ingestFile() {
 
+  }
+
+  cleanDragAndDrop() {
+    this.m_oFile = null;
   }
   onDismiss() {
     this.m_oDialogRef.close();
