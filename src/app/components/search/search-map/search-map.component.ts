0f6fec92a1f1bf2a3b2d6e0102c05401f080fdf7
Betty <betty.spurgeon@wasdi.cloud>
2023-09-05 15:14:52 +0200

    Create search map component

---
+++ src/app/components/search/search-map/search-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-map',
+  templateUrl: './search-map.component.html',
+  styleUrls: ['./search-map.component.css']
+})
+export class SearchMapComponent {
+
+}
