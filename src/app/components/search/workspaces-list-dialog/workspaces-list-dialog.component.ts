c70ac342db05dd67030539bcd5a068962bd2076a
Betty <betty.spurgeon@wasdi.cloud>
2023-09-19 12:05:38 +0200

    Create workspace list dialog

---
+++ src/app/components/search/workspaces-list-dialog/workspaces-list-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workspaces-list-dialog',
+  templateUrl: './workspaces-list-dialog.component.html',
+  styleUrls: ['./workspaces-list-dialog.component.css']
+})
+export class WorkspacesListDialogComponent {
+
+}
