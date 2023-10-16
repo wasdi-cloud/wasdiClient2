b17aecdfe3e262aa687ae1c944e188bcc417551f
pcampanella <p.campanella@fadeout.it>
2023-10-16 19:35:37 +0200

    debug of the rabbit service

--- src/app/services/rabbit-stomp.service.ts
+++ src/app/services/rabbit-stomp.service.ts
@@ -26,28 +26,58 @@ export class RabbitStompService {
   constructor(
     private m_oConstantsService: ConstantsService,
     private m_oNotificationDisplayService: NotificationDisplayService,
-    private m_oProcessWorkspaceService: ProcessWorkspaceService) { }
+    private m_oProcessWorkspaceService: ProcessWorkspaceService) { 
+      console.log("CREATED ISTANCE OF RabbitStompService")
+    }
 
-  // Translate.m_oTranslate = oTranslate;
-
+  /**
+   * Web Socket object
+   */
   m_oWebSocket = null;
-  m_oReconnectTimerPromise = null;
-  m_oRabbitReconnectAttemptCount = 0;
 
-  // STOMP Client
+  /**
+   * Flag to know if we are reconnecting to rabbit (!= null) or not (=null)
+   */
+  m_oReconnectTimerFlag = null;
+
+  /**
+   * Counter of the rabbit reconnection attemps
+   */
+  m_iRabbitReconnectAttemptCount = 0;
+
+  /**
+   * STOMP Client
+   */
   m_oClient = null;
-  // Rabbit Connect Callback
-  m_oOn_Connect = null;
-  // Rabbit Error Callback
+
+  /**
+   *  Rabbit Connect Callback
+   */
+  m_oOn_Connected = null;
+  
+  /**
+   * Rabbit Error Callback
+   */
   m_oOn_Error = null;
-  // Rabbit Reconnect Callback
+
+  /**
+   * Rabbit Reconnect Callback
+   */
   m_oRabbitReconnect = null;
 
+  /**
+   * Subscription to the queue
+   */
   m_oSubscription = null;
+
+  /**
+   * Actual WASDI User
+   */
   m_oUser = null;
 
-  m_aoErrorsList = [];
-
+  /**
+   * Callback for the received message event
+   */
   m_fMessageCallBack = null;
 
   /**
@@ -77,15 +107,30 @@ export class RabbitStompService {
    */
   m_sCallOnce = "CALL_ONCE";
 
+  /**
+   * Reference to the controller active in this moment
+   */
   m_oActiveController = null;
 
+  /**
+   * Actual Workspace Id
+   */
   m_sWorkspaceId = "";
 
+  /**
+   * Connection state. Can be 
+   * Init = 0,
+   * Connected = 1,
+   * Lost = 2
+   * Used in the client to show the status of the asynch communication channel
+   */
   m_iConnectionState = new BehaviorSubject<number>(RabbitConnectionState.Init);
   _m_iConnectionState$ = this.m_iConnectionState.asObservable();
 
 
-  // Use defer/promise to keep trace when service is ready to perform any operation
+  /**
+   * Use defer/promise to keep trace when service is ready to perform any operation
+   */
   m_oServiceReadyDeferred = null;
   m_oServiceReadyPromise = null;
 
@@ -103,10 +148,6 @@ export class RabbitStompService {
   setActiveController(oController) {
     this.m_oActiveController = oController;
   }
-
-  // addMessageHook(sMessageType, oController, fCallback) {
-  //   return this.addMessageHook(sMessageType, this.m_oConstantsService, fCallback, false);
-  // }
 
   /**
    * Adds a Message Hook to a message type.
@@ -139,41 +180,45 @@ export class RabbitStompService {
     return !FadeoutUtils.utilsIsStrNullOrEmpty(this.m_sWorkspaceId);
   }
 
-  notifyConnectionStateChange(connectionState: number) {
-    return this.m_iConnectionState.next(connectionState);
-    //let msgHlp = MessageHelper.getInstance($rootScope);
-    //msgHlp.notifyRabbitConnectionStateChange(connectionState);
+  notifyConnectionStateChange(iConnectionState: number) {
+    return this.m_iConnectionState.next(iConnectionState);
   }
-
 
   getConnectionState() {
     return this._m_iConnectionState$;
   }
 
-
   isReadyState() {
     return (((FadeoutUtils.utilsIsObjectNullOrUndefined(this.m_oWebSocket) === false) && (this.m_oWebSocket.readyState === WebSocket.OPEN)) ? true : false);
   }
 
-  subscribe(workspaceId) {
+  /**
+   * Subscribe the client to a queue in Rabbit.
+   * Each workspace is a queue.
+   * 
+   * @param sWorkspaceId Workspace Id = Queue Id
+   */
+  subscribe(sWorkspaceId) {
 
     //this.unsubscribe();
 
-    this.m_sWorkspaceId = workspaceId;
+    this.m_sWorkspaceId = sWorkspaceId;
 
-    let subscriptionString = "/exchange/amq.topic/" + workspaceId;
-    console.log("RabbitStompService: subscribing to " + subscriptionString);
+    let sSubscriptionString = "/exchange/amq.topic/" + sWorkspaceId;
+    console.log("RabbitStompService: subscribing to " + sSubscriptionString);
     let oThisService = this;
 
     try {
-      this.m_oSubscription = this.m_oClient.subscribe(subscriptionString, function (message) {
+      this.m_oSubscription = this.m_oClient.subscribe(sSubscriptionString, function (oMessage) {
+
+        // THIS IS the function executed when we receive a message
 
         // Check message Body
-        if (message.body) {
-          console.log("RabbitStompService: got message with body " + message.body)
+        if (oMessage.body) {
+          console.log("RabbitStompService: got message with body " + oMessage.body)
 
           // Get The Message View Model
-          let oMessageResult = JSON.parse(message.body);
+          let oMessageResult = JSON.parse(oMessage.body);
 
           // Check parsed object
           if (oMessageResult == null) {
@@ -208,7 +253,7 @@ export class RabbitStompService {
 
           //Reject the message if it is for another workspace
           if (oMessageResult.workspaceId != sActiveWorkspaceId) {
-            //return false;
+            return;
           }
 
           // Check if the callback is hooked
@@ -223,7 +268,7 @@ export class RabbitStompService {
       });
     }
     catch (e) {
-      console.log("RabbitStompService: Exception subscribing to " + workspaceId);
+      console.log("RabbitStompService: Exception subscribing to " + sWorkspaceId);
     }
   };
 
@@ -235,48 +280,59 @@ export class RabbitStompService {
   };
 
   initWebStomp() {
-    let _this = this;
 
+    console.log("RabbitStompService: called initWebStomp");
+
+    /**
+     * Reference to the service class
+     */
+    let m_oThisReference = this;
     this.m_oServiceReadyDeferred = new Promise(() => { });
 
     // Web Socket to receive workspace messages
     let oWebSocket = new WebSocket(this.m_oConstantsService.getStompUrl());
-    //let oWebSocket = new WebSocket(this.m_oConstantsService.getStompUrl());
+
     this.m_oClient = Stomp.over(oWebSocket);
     this.m_oClient.heartbeat.outgoing = 20000;
     this.m_oClient.heartbeat.incoming = 20000;
     this.m_oClient.debug = null;
 
     /**
-     * Callback of the Rabbit On Connect
+     * Callback of the Rabbit On Connect event
      */
-    let on_connect = function () {
-      console.log('RabbitStompService: Web Stomp connected');
+    let on_connected = function () {
+      console.log('RabbitStompService.on_connect: Web Stomp connected');
+      
+      // We are connected to the server!!
+      m_oThisReference.notifyConnectionStateChange(RabbitConnectionState.Connected);
+      // For sure we can clean this counter (but will be cleaned also in case of "first" error)
+      m_oThisReference.m_iRabbitReconnectAttemptCount = 0;
 
-      _this.notifyConnectionStateChange(RabbitConnectionState.Connected);
-      _this.m_oRabbitReconnectAttemptCount = 0;
-
-      //CHECK IF the session is valid
-      let oSessionId = _this.m_oConstantsService.getSessionId();
+      //Check if the session is valid
+      let oSessionId = m_oThisReference.m_oConstantsService.getSessionId();
 
       if (FadeoutUtils.utilsIsObjectNullOrUndefined(oSessionId)) {
-        console.log("RabbitStompService: Error session id Null in on_connect");
+        // Well no-user no-rabbit
+        console.log("RabbitStompService.on_connect: Error session id Null in on_connect");
         return false;
       }
 
-      //_this.m_oServiceReadyDeferred.resolve(true);
+      // Is this callback called from re-connection or is the real first connection?
+      if (m_oThisReference.m_oReconnectTimerFlag != null) {
+        // It was a reconnection, because we have a value of m_oReconnectTimerPromise
+        console.log("RabbitStompService.on_connect: it is a re-connection, clean the flag");
 
-      // Is this a re-connection?
-      if (_this.m_oReconnectTimerPromise != null) {
+        // Clear the timer flag
+        m_oThisReference.m_oReconnectTimerFlag = null;
 
-        // Yes it is: clear the timer
-        //_this.m_oInterval.cancel(_this.m_oReconnectTimerPromise);
-        _this.m_oReconnectTimerPromise = null;
-
-        if (_this.m_sWorkspaceId !== "") {
-          _this.subscribe(_this.m_sWorkspaceId);
+        // Do we have also an open workspace?
+        if (m_oThisReference.m_sWorkspaceId !== "") {
+          // Yes, so we need also to re-subscribe the queue of this workspace
+          console.log("RabbitStompService.on_connect: it is a re-connection, re-subscribe to the workspace");
+          m_oThisReference.subscribe(m_oThisReference.m_sWorkspaceId);
         }
       }
+
       return true;
     };
 
@@ -286,8 +342,9 @@ export class RabbitStompService {
      */
     let on_error = function (sMessage) {
 
-      console.log('RabbitStompService: WEB STOMP ERROR, message:' + sMessage + ' [' + FadeoutUtils.utilsGetTimeStamp() + ']');
+      console.log('RabbitStompService.on_error: WEB STOMP ERROR, message:' + sMessage + ' [' + FadeoutUtils.utilsGetTimeStamp() + ']');
 
+      // Message can be a string or an object: here we get the string in a safe way
       if (!(typeof sMessage === 'string' || sMessage instanceof String)) {
         if (!FadeoutUtils.utilsIsObjectNullOrUndefined(sMessage.body)) {
           sMessage = sMessage.body;
@@ -297,52 +354,66 @@ export class RabbitStompService {
         }
       }
 
+      // Is this a lost connection Message?
       if (sMessage == "LOST_CONNECTION" || sMessage.includes("Whoops! Lost connection to")) {
-        console.log('RabbitStompService: Web Socket Connection Lost');
+        // Log
+        console.log('RabbitStompService.on_error: Web Socket Connection Lost');
+        // Change the status of the connection, so that we can change the icon for our users
+        m_oThisReference.notifyConnectionStateChange(RabbitConnectionState.Lost);
 
-        _this.notifyConnectionStateChange(RabbitConnectionState.Lost);
-
-        if (_this.m_oReconnectTimerPromise == null) {
-          // Try to Reconnect
-          _this.m_oRabbitReconnectAttemptCount = 0;
-          _this.m_oReconnectTimerPromise = setInterval(_this.m_oRabbitReconnect, 5000);
+        // Is this the first consecutive error we receive?
+        if (m_oThisReference.m_oReconnectTimerFlag == null) {
+          // Yes, it is
+          console.log('RabbitStompService.on_error: reconnection timer promise is  null, clean Attemp count');
+          // Restart our Reconnection counter to zero
+          m_oThisReference.m_iRabbitReconnectAttemptCount = 0;
         }
+        else {
+          // This will be deleted, just to understand what is happening
+          console.log('RabbitStompService.on_error: reconnection timer promise is already not null ' + m_oThisReference.m_oReconnectTimerFlag);
+        }
+
+          // P.Campanella 16/10/2023: call set Timeout to run rabbit_reconnect once in 5 secs. And keep the return code
+          // That we will use to understand if this is the first attemp or not
+          m_oThisReference.m_oReconnectTimerFlag = setTimeout(m_oThisReference.m_oRabbitReconnect, 5000);
+
+          console.log('RabbitStompService.on_error: reconnection timer promise now is = ' + m_oThisReference.m_oReconnectTimerFlag);
       }
     };
 
     // Keep local reference to the callbacks to use it in the reconnection callback
-    this.m_oOn_Connect = on_connect;
+    this.m_oOn_Connected = on_connected;
     this.m_oOn_Error = on_error;
     this.m_oWebSocket = oWebSocket;
-    //Call back for rabbit reconnection
-    let rabbit_reconnect = function () {
 
-      _this.m_oRabbitReconnectAttemptCount++;
-      console.log('RabbitStompService: Web Stomp Reconnection Attempt (' + _this.m_oRabbitReconnectAttemptCount + ')');
+    /**
+     * Call back for rabbit reconnection
+     */
+    let rabbit_reconnect = function () {
+      // Increase the count of attemps
+      m_oThisReference.m_iRabbitReconnectAttemptCount++;
+      console.log('RabbitStompService: Web Stomp Reconnection Attempt (' + m_oThisReference.m_iRabbitReconnectAttemptCount + ')');
 
       // Connect again
-      _this.m_oWebSocket = new WebSocket(_this.m_oConstantsService.getStompUrl());
+      m_oThisReference.m_oWebSocket = new WebSocket(m_oThisReference.m_oConstantsService.getStompUrl());
       //_this.oWebSocket = new SockJS(_this.m_oConstantsService.getStompUrl());
-      _this.m_oClient = Stomp.over(_this.m_oWebSocket);
-      _this.m_oClient.heartbeat.outgoing = 20000;
-      _this.m_oClient.heartbeat.incoming = 20000;
-      _this.m_oClient.debug = null;
+      m_oThisReference.m_oClient = Stomp.over(m_oThisReference.m_oWebSocket);
+      m_oThisReference.m_oClient.heartbeat.outgoing = 20000;
+      m_oThisReference.m_oClient.heartbeat.incoming = 20000;
+      m_oThisReference.m_oClient.debug = null;
 
-      _this.m_oClient.connect(_this.m_oConstantsService.getRabbitUser(), _this.m_oConstantsService.getRabbitPassword(), _this.m_oOn_Connect, _this.m_oOn_Error, '/');
+      // Call connect: one of m_oOn_Connected or m_oOn_Error function will be called
+      m_oThisReference.m_oClient.connect(m_oThisReference.m_oConstantsService.getRabbitUser(), m_oThisReference.m_oConstantsService.getRabbitPassword(), m_oThisReference.m_oOn_Connected, m_oThisReference.m_oOn_Error, '/');
     };
 
+    // Save the reference to the rabbit_reconnect callback, we will reuse it in case of errors
     this.m_oRabbitReconnect = rabbit_reconnect;
-    //connect to the queue
-    this.m_oClient.connect(_this.m_oConstantsService.getRabbitUser(), _this.m_oConstantsService.getRabbitPassword(), on_connect, on_error, '/');
+
+    // Connect to the rabbit server
+    this.m_oClient.connect(m_oThisReference.m_oConstantsService.getRabbitUser(), m_oThisReference.m_oConstantsService.getRabbitPassword(), on_connected, on_error, '/');
 
     return true;
   };
-
-  // receivedNewProductMessage(oMessage: any) {
-  //   let sMessage: string = "PRODUCT ADDED";
-
-  //   console.log(sMessage);
-  // }
 
   /**
    * Handler of the "Publish" message
