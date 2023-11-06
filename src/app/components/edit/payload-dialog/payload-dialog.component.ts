55d9a17cfd8fbed3a79ce8a9c9e121f53d7df4f3
Betty <betty.spurgeon@wasdi.cloud>
2023-11-06 09:01:27 +0100

    #43 Fix tabs selection and add functionality for bar charts

--- src/app/components/edit/payload-dialog/payload-dialog.component.ts
+++ src/app/components/edit/payload-dialog/payload-dialog.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { LegendPosition } from '@swimlane/ngx-charts';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 import { AlertDialogTopService } from 'src/app/services/alert-dialog-top.service';
+import FadeoutUtils from 'src/app/lib/utils/FadeoutJSUtils';
 
 
 
@@ -14,6 +15,7 @@ import { AlertDialogTopService } from 'src/app/services/alert-dialog-top.service
 export class PayloadDialogComponent implements OnInit {
   faXmark = faX;
   m_oProcess = this.m_oData.process;
+  m_sActiveTab: string = "payload"
   m_sPayloadString: string = "";
   m_bIsWasdiDasboard: boolean = false;
   m_sGraphType: string;
@@ -119,11 +121,18 @@ export class PayloadDialogComponent implements OnInit {
       this.m_bIsWasdiDasboard = true;
       console.log(oPayload.wasdi_dashboard)
       this.m_aoChartsInformation = oPayload.wasdi_dashboard;
+      this.m_sActiveTab = 'dashboard';
+      console.log(this.m_aoChartsInformation);
     } else {
       this.getPayloadString();
     }
   }
 
+  setActiveTab(sActiveTab: string) {
+    if (FadeoutUtils.utilsIsStrNullOrEmpty(sActiveTab) === false) {
+      this.m_sActiveTab = sActiveTab;
+    }
+  }
 
   dismiss(event: MouseEvent) {
     this.m_oDialog.closeAll();
