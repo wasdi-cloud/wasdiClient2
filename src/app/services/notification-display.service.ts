f1b30a28b17a2398fe5810b059e2b800f59bf99b
Betty <betty.spurgeon@wasdi.cloud>
2023-11-22 15:12:58 +0100

    Add alert dialog to notification service and refactor snackbar

--- src/app/services/notification-display.service.ts
+++ src/app/services/notification-display.service.ts
@@ -1,23 +1,60 @@
 import { Injectable } from '@angular/core';
+
+import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { AlertDialogComponent } from 'src/app/shared/dialogs/alert-dialog/alert-dialog.component';
+import { NotificationSnackbarComponent } from 'src/app/shared/dialogs/notification-snackbar/notification-snackbar.component';
+import { ConfirmationDialogComponent } from 'src/app/shared/dialogs/confirmation-dialog/confirmation-dialog.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationDisplayService {
 
-  constructor(private m_oMatSnackBar: MatSnackBar) { }
+  constructor(
+    private m_oMatDialog: MatDialog,
+    private m_oMatSnackBar: MatSnackBar) { }
 
+  /**
+   * Handler to 
+   * @param sMessage 
+   * @param sAction 
+   * @param hPosition 
+   * @param vPosition 
+   * @param className 
+   */
   openSnackBar(sMessage: string, sAction: string,
-    hPosition?: any, vPosition?: any,
-    className?: any) {
-    this.m_oMatSnackBar.open(sMessage, sAction, {
+    hPosition?: any, vPosition?: any, className?: string) {
+    this.m_oMatSnackBar.openFromComponent(NotificationSnackbarComponent, {
       duration: 4000,
-      horizontalPosition: hPosition ? hPosition : 'center',
-      verticalPosition: vPosition ? vPosition : 'top',
-      panelClass: className
-      // direction: "rtl"
+      horizontalPosition: hPosition ? hPosition : 'right',
+      verticalPosition: vPosition ? vPosition : 'bottom',
+      panelClass: className,
+      data: sMessage
     });
   }
 
+  /**
+   * Handler to open the alert dialog componenet 
+   * @param sMessage 
+   * @param iTimeoutInput 
+   */
+  openAlertDialog(sMessage: string, iTimeoutInput?: number): void {
+    //Set default 4 second timeout to close alert dialog
+    let iTimeout = 4000;
+    if (iTimeoutInput) {
+      iTimeout = iTimeoutInput;
+    }
+    let oDialogRef = this.m_oMatDialog.open(AlertDialogComponent, {
+      data: sMessage
+    });
+
+    //Set Automatic timeout for dialog
+    oDialogRef.afterOpened().subscribe(oResponse => {
+      setTimeout(() => {
+        oDialogRef.close();
+      }, iTimeout)
+    });
+  }
 }
