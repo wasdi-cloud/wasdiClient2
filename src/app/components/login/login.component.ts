a89f7e9220e126fb9a51277862c6e18fbed7dda4
Betty <betty.spurgeon@wasdi.cloud>
2024-01-03 17:51:29 +0100

    Add keycloak option to login and register

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,6 +6,8 @@ import { User } from 'src/app/shared/models/user.model';
 
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent, ErrorDialogModel } from 'src/app/shared/dialogs/error-dialog/error-dialog.component';
+import { KeycloakService } from 'keycloak-angular';
+import { initializeKeycloak } from 'src/app/init/keycloak-init.factory';
 
 @Component({
   selector: 'app-login',
@@ -26,9 +28,13 @@ export class LoginComponent implements OnInit {
     private m_oAuthService: AuthService,
     private m_oDialog: MatDialog,
     private m_oConstantsService: ConstantsService,
+    private m_oKeycloakService: KeycloakService,
     private m_oRouter: Router) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.checkKeycloakAuthStatus(this);
+    console.log(initializeKeycloak);
+  }
 
   login(): void {
     const { username, password } = this.form;
@@ -40,6 +46,15 @@ export class LoginComponent implements OnInit {
     this.m_oAuthService.legacyLogin(oLoginInfo).subscribe((oResponse => {
       this.callbackLogin(oResponse, this)
     }))
+  }
+
+  checkKeycloakAuthStatus(oController) {
+    if (this.m_oKeycloakService.isLoggedIn) {
+      if (this.m_oKeycloakService.getToken()) {
+        console.log(this.m_oKeycloakService.getToken());
+        console.log(this.m_oKeycloakService.getKeycloakInstance())
+      }
+    }
   }
 
   /**
@@ -61,7 +76,7 @@ export class LoginComponent implements OnInit {
         let dialogRef = this.m_oDialog.open(ErrorDialogComponent, {
           maxWidth: "400px",
           data: oDialogData
-        })  
+        })
       }
       else {
         // Ok we have a valid session Id
@@ -74,16 +89,26 @@ export class LoginComponent implements OnInit {
         oUser.role = data.role;
         oUser.type = data.type;
         oUser.grantedAuthorities = data.grantedAuthorities;
-  
+
         //set user and cookie
         this.m_oConstantsService.setUser(oUser);
         this.m_oAuthService.saveToken(data.sessionId);
 
-        this.m_oAuthService.checkSession().subscribe({next: oResponse =>  {
-          oController.m_oRouter.navigateByUrl('/marketplace');
-        }})
-        
+        this.m_oAuthService.checkSession().subscribe({
+          next: oResponse => {
+            oController.m_oRouter.navigateByUrl('/marketplace');
+          }
+        })
+
       }
     }
+  }
+
+  keycloakLogin() {
+    this.m_oKeycloakService.login();
+  }
+
+  keycloakRegister() {
+    this.m_oKeycloakService.register();
   }
 }
