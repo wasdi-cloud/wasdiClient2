73f1bd7dbce70847acb6888031591cfd2ffa7a40
Betty <betty.spurgeon@wasdi.cloud>
2024-01-30 14:48:56 +0100

    Add project select to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,6 +4,10 @@ import { ConstantsService } from 'src/app/services/constants.service';
 
 import { User } from 'src/app/shared/models/user.model';
 import { UserSettingsDialogComponent } from './header-dialogs/user-settings-dialog/user-settings-dialog.component';
+import { ProjectService } from 'src/app/services/api/project.service';
+
+import FadeoutUtils from 'src/app/lib/utils/FadeoutJSUtils';
+import { NotificationDisplayService } from 'src/app/services/notification-display.service';
 
 @Component({
   selector: 'app-header',
@@ -12,52 +16,135 @@ import { UserSettingsDialogComponent } from './header-dialogs/user-settings-dial
 })
 export class HeaderComponent implements OnInit {
   m_oUser: User;
+  m_sActiveProjectName: string = "";
+  m_aoUserProjects: Array<any> = [];
+  m_aoUserProjectsMap: Array<any> = [];
+  m_oProject: any;
+  m_oSelectedProject: any = { name: "No Active Project", projectId: null };
+
+
   constructor(
     private m_oConstantsService: ConstantsService,
+    private m_oNotificationDisplayService: NotificationDisplayService,
+    private m_oProjectService: ProjectService,
     private m_oDialog: MatDialog
   ) { }
 
   ngOnInit(): void {
-   this.m_oUser = this.m_oConstantsService.getUser()
+    this.m_oUser = this.m_oConstantsService.getUser();
+    this.initializeProjectsInfo();
+    this.m_oProjectService.getProjectsListByUser().subscribe({
+      next: oResponse => { },
+      error: oError => { }
+    })
   }
+
+
+  initializeProjectsInfo() {
+    let oFirstProjectElement = { name: "No Active Project", projectId: null };
+
+    this.m_oProjectService.getValidProjectsListByUser().subscribe({
+      next: oResponse => {
+        if (FadeoutUtils.utilsIsObjectNullOrUndefined(oResponse) === false) {
+          this.m_aoUserProjects = oResponse;
+
+          let aoProjects = [oFirstProjectElement].concat(oResponse);
+
+          this.m_aoUserProjectsMap = aoProjects.map(oProject => {
+            return ({ name: oProject.name, projectId: oProject.projectId });
+          });
+          let asSubscriptionNames: Array<string> = [];
+
+          this.m_oProject = oFirstProjectElement;
+
+          this.m_aoUserProjects.forEach((oValue) => {
+            //Add subscription name to the array
+            asSubscriptionNames.push(oValue.subscriptionName);
+            //FRONTEND FIX FOR NO ACTIVE PROJECT SELECT: 
+            if (oValue.projectId === this.m_oSelectedProject.projectId) {
+              this.m_oSelectedProject = oFirstProjectElement;
+            }
+            if (oValue.activeProject) {
+              this.m_oSelectedProject = oValue;
+              this.m_oConstantsService.setActiveProject(oValue);
+            };
+          });
+
+          this.m_oConstantsService.setActiveSubscriptions(asSubscriptionNames);
+          if (FadeoutUtils.utilsIsObjectNullOrUndefined(this.m_oSelectedProject)) {
+            this.m_oSelectedProject = this.m_aoUserProjectsMap[0];
+          }
+          // this.m_bLoadingProjects = false;
+        } else {
+          // this.m_oAlertDialog.openDialog(4000, "Error in getting your projects");
+          // this.m_bLoadingProjects = false;
+          this.m_oProject = oFirstProjectElement;
+
+        }
+      },
+      error: oError => {
+        let sErrorMessage = "Error in getting your projects";
+        this.m_oProject = oFirstProjectElement;
+        // this.m_oAlertDialog.openDialog(4000, sErrorMessage);
+      }
+    });
+  }
+
+  setActiveProject(oProject: any) {
+    if (FadeoutUtils.utilsIsObjectNullOrUndefined(oProject) === false) {
+      this.m_oProjectService.changeActiveProject(oProject.projectId).subscribe({
+        next: oResponse => {
+          if (FadeoutUtils.utilsIsObjectNullOrUndefined(oResponse) === false) {
+            this.m_oNotificationDisplayService.openSnackBar("Active Project Changed", "Close", "right", "bottom");
+
+            this.m_oSelectedProject = oProject;
+            this.m_oConstantsService.setActiveProject(oProject);
+            this.initializeProjectsInfo();
+          }
+        },
+        error: oError => { }
+      });
+    }
+  }
+
 
   openUserDashboard() {
     this.m_oDialog.open(UserSettingsDialogComponent);
   }
 }
 
-  // m_oFeedback: {
-  //   title: string | null,
-  //   message: string | null
-  // } = { title: null, message: null }
-  // constructor(
-  //   private m_oFeedbackService: FeedbackService,
-  // ) {
-  //   //Register translation languages:
-  //   m_oTranslate.addLangs(['en', 'es', 'fr', 'it', 'de', 'vi', 'id', 'ro']);
-  //   m_oTranslate.setDefaultLang('en');
+// m_oFeedback: {
+//   title: string | null,
+//   message: string | null
+// } = { title: null, message: null }
+// constructor(
+//   private m_oFeedbackService: FeedbackService,
+// ) {
+//   //Register translation languages:
+//   m_oTranslate.addLangs(['en', 'es', 'fr', 'it', 'de', 'vi', 'id', 'ro']);
+//   m_oTranslate.setDefaultLang('en');
 
-  // logout() {
-  //   this.m_oConstantsService.logOut();
-  //   this.m_oRouter.navigateByUrl("login");
-  // }
+// logout() {
+//   this.m_oConstantsService.logOut();
+//   this.m_oRouter.navigateByUrl("login");
+// }
 
-  // sendFeedback() {
-  //   if (typeof this.m_oFeedback === undefined || !this.m_oFeedback.title || !this.m_oFeedback.message) {
-  //     console.log("Error sending message");
-  //     return false;
-  //   }
+// sendFeedback() {
+//   if (typeof this.m_oFeedback === undefined || !this.m_oFeedback.title || !this.m_oFeedback.message) {
+//     console.log("Error sending message");
+//     return false;
+//   }
 
-  //   this.m_oFeedbackService.sendFeedback(this.m_oFeedback).subscribe(oResponse => {
-  //     if (typeof oResponse !== null && typeof oResponse !== undefined && oResponse.boolValue === true) {
-  //       console.log("feedback sent")
-  //       return true;
-  //     } else {
-  //       console.log("error sending feedback");
-  //       return false;
-  //     }
-  //   });
-  //   return true;
-  // }
+//   this.m_oFeedbackService.sendFeedback(this.m_oFeedback).subscribe(oResponse => {
+//     if (typeof oResponse !== null && typeof oResponse !== undefined && oResponse.boolValue === true) {
+//       console.log("feedback sent")
+//       return true;
+//     } else {
+//       console.log("error sending feedback");
+//       return false;
+//     }
+//   });
+//   return true;
+// }
 
 
