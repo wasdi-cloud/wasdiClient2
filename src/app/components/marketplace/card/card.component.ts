5d77aad69c504c5447ceaf7d958f7596b8af76c0
Betty <betty.spurgeon@wasdi.cloud>
2024-01-31 09:53:15 +0100

    Open app from marketplace

--- src/app/components/marketplace/card/card.component.ts
+++ src/app/components/marketplace/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConstantsService } from 'src/app/services/constants.service';
 
 @Component({
   selector: 'app-card',
@@ -10,4 +12,14 @@ export class CardComponent {
    * The input object of the processor. 
    */
   @Input() m_oProcessor: any = {}
+
+  constructor(
+    private m_oConstantsService: ConstantsService, 
+    private m_oRouter: Router
+  ) { }
+
+  openProcessorDetails(sProcessorName: string) {
+    this.m_oConstantsService.setSelectedApplication(sProcessorName)
+    this.m_oRouter.navigateByUrl(`${sProcessorName}/appDetails`)
+  }
 }
