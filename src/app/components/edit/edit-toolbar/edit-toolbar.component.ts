dd99b7306e31f2a5295ba41bac3ea640bf78d4a8
Betty <betty.spurgeon@wasdi.cloud>
2024-03-06 12:29:43 +0100

    Clean edit toolbar and apply new style

--- src/app/components/edit/edit-toolbar/edit-toolbar.component.ts
+++ src/app/components/edit/edit-toolbar/edit-toolbar.component.ts
@@ -3,9 +3,7 @@ import { ConsoleService } from 'src/app/services/api/console.service';
 import { ConstantsService } from 'src/app/services/constants.service';
 import { Workspace } from 'src/app/shared/models/workspace.model';
 
-import { faSearch, faRefresh, faInfoCircle, faRocket, faPlusSquare, faGears, faCloudUpload, faPaintBrush, faShareNodes, faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 import { MatDialog } from '@angular/material/dialog';
-import { WorkspaceInfoDialogComponent } from '../workspace-info-dialog/workspace-info-dialog.component';
 import { ShareDialogComponent, ShareDialogModel } from 'src/app/shared/dialogs/share-dialog/share-dialog.component';
 import { StylesDialogComponent } from './toolbar-dialogs/styles-dialog/styles-dialog.component';
 import { ImportDialogComponent } from './toolbar-dialogs/import-dialog/import-dialog.component';
@@ -25,18 +23,6 @@ import { NotificationDisplayService } from 'src/app/services/notification-displa
   styleUrls: ['./edit-toolbar.component.css']
 })
 export class EditToolbarComponent implements OnInit, OnDestroy {
-  //Font Awesome Icons: 
-  faSearch = faSearch;
-  faRefresh = faRefresh;
-  faInfo = faInfoCircle;
-  faRocket = faRocket;
-  faPlus = faPlusSquare;
-  faGears = faGears;
-  faUpload = faCloudUpload;
-  faComputer = faLaptopCode;
-  faStyles = faPaintBrush;
-  faShare = faShareNodes;
-
   @Input() m_oActiveWorkspace: Workspace;
   @Input() m_aoProducts: Product[];
   @Input() m_bJupyterIsReady: boolean = false;
@@ -60,48 +46,24 @@ export class EditToolbarComponent implements OnInit, OnDestroy {
     this.m_iHookIndex = this.m_oRabbitStompService.addMessageHook("LAUNCHJUPYTERNOTEBOOK",
       this,
       this.rabbitMessageHook, true)
+    this.m_oActiveWorkspace = this.m_oConstantsService.getActiveWorkspace();
   }
 
   ngOnDestroy(): void {
     this.m_oRabbitStompService.removeMessageHook(this.m_iHookIndex);
   }
 
-  openWorkspaceInfo(event: MouseEvent) {
-    let oDialogRef = this.m_oDialog.open(WorkspaceInfoDialogComponent, {
-      width: '60vw',
-      data: this.m_oActiveWorkspace
-    });
-  }
 
-
-  getFilterText() {
-    if (this.m_sFilterText === undefined || this.m_sFilterText === null) {
-      this.m_sFilterText = "";
-    }
-    this.m_sSearchString.emit(this.m_sFilterText);
-  }
-
-  resetFilterText(event) {
-    event.preventDefault();
-    this.m_sFilterText = undefined;
-    this.m_sSearchString.emit("");
-  }
-
-  /**
-   * Secondary Toolbar Options
-   */
-  openAppsDialog(oEvent: MouseEvent) {
-    oEvent.preventDefault();
+  openAppsDialog(): void {
     this.m_oDialog.open(AppsDialogComponent, {
       height: '90vh',
       width: '90vw',
       maxWidth: '100vw',
-      minWidth:'90vw'
+      minWidth: '90vw'
     })
   }
 
-  openWorkflowsDialog(event: MouseEvent) {
-    event.preventDefault();
+  openWorkflowsDialog(): void {
     this.m_oDialog.open(WorkflowsDialogComponent, {
       height: '80vh',
       width: '80vw',
@@ -111,8 +73,7 @@ export class EditToolbarComponent implements OnInit, OnDestroy {
     })
   }
 
-  openNewAppDialog(oEvent: MouseEvent) {
-    oEvent.preventDefault();
+  openNewAppDialog(): void {
     this.m_oDialog.open(NewAppDialogComponent, {
       height: '90vh',
       width: '90vw',
@@ -121,15 +82,14 @@ export class EditToolbarComponent implements OnInit, OnDestroy {
 
   }
 
-  openImportDialog(oEvent: MouseEvent) {
-    let oDialog = this.m_oDialog.open(ImportDialogComponent, {
+  openImportDialog(): void {
+    this.m_oDialog.open(ImportDialogComponent, {
       height: '60vh',
       width: '50vw'
     })
   }
 
-  openJupyterNotebookPage(oEvent: MouseEvent) {
-    oEvent.preventDefault
+  openJupyterNotebookPage(): boolean {
     //Check if user has valid subscription and active project
     if (this.m_oConstantsService.checkProjectSubscriptionsValid() === false) {
       return false;
@@ -145,31 +105,29 @@ export class EditToolbarComponent implements OnInit, OnDestroy {
             sMessage = sMessage + "<BR>" + oResponse.stringValue;
             this.m_oNotificationDisplayService.openAlertDialog(sMessage);
           }
-          }
+        }
 
       });
       return true;
     }
   }
 
-  openStylesDialog(oEvent: MouseEvent) {
-    oEvent.preventDefault();
-    let dialogRef = this.m_oDialog.open(StylesDialogComponent, {
+  openStylesDialog(): void {
+    this.m_oDialog.open(StylesDialogComponent, {
       height: '80vh',
       width: '80vw'
     })
   }
 
-  openShareDialog(event: MouseEvent) {
-    event.preventDefault();
+  openShareDialog(): void {
     let dialogData = new ShareDialogModel("workspace", this.m_oActiveWorkspace)
-    let dialogRef = this.m_oDialog.open(ShareDialogComponent, {
+    this.m_oDialog.open(ShareDialogComponent, {
       width: '50vw',
       data: dialogData
     });
   }
 
-  rabbitMessageHook(oRabbitMessage, oController) {
+  rabbitMessageHook(oRabbitMessage, oController): void {
     if (oRabbitMessage.messageResult === "OK") {
       oController.m_bJupyterIsReady = true;
     }
