a39c7468d8dc1d1b9ab958c3aa4c5294ae64e15f
Betty <betty.spurgeon@wasdi.cloud>
2024-04-02 11:14:21 +0200

    #138 added delete icon and user id to workspaces autocomplete

--- src/app/shared/shared-components/autocomplete/autocomplete.component.ts
+++ src/app/shared/shared-components/autocomplete/autocomplete.component.ts
@@ -28,6 +28,7 @@ export class AutocompleteComponent implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     this.m_sSearchString = "";
+    console.log(this.m_aoInputs)
   }
 
   getOptionText(oOption) {
