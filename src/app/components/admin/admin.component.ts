bde4ad0781070c0416fda69260a7fb6dea68b380
Betty <betty.spurgeon@wasdi.cloud>
2024-04-17 15:12:47 +0200

    get organizations in parent

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NotificationDisplayService } from 'src/app/services/notification-display.service';
+import { OrganizationsService } from 'src/app/services/api/organizations.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css'],
   host: { 'class': 'flex-fill' }
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
   m_sActiveTab: string = 'account';
+
+  m_aoOrganizations
+
+  constructor(private m_oNotificationDisplayService: NotificationDisplayService,
+    private m_oOrganizationsService: OrganizationsService,) { }
+
+  ngOnInit(): void {
+    this.getOrganizationsListByUser();
+  }
 
   getActiveTab(sEvent: string) {
     this.m_sActiveTab = sEvent;
+  }
+
+  getOrganizationsListByUser() {
+    this.m_oOrganizationsService.getOrganizationsListByUser().subscribe({
+      next: oResponse => {
+        if (oResponse.status !== 200) {
+          this.m_oNotificationDisplayService.openAlertDialog("ERROR IN FETCHING ORGANIZATIONS");
+        } else {
+          const oFirstElement = { name: "No Organization", organizationId: null };
+          this.m_aoOrganizations = [oFirstElement].concat(oResponse.body);
+          console.log(this.m_aoOrganizations)
+          // this.m_aoOrganizationsMap = this.m_asOrganizations.map(
+          //   (item) => ({ name: item.name, organizationId: item.organizationId })
+          // );
+
+          // this.m_aoOrganizationsMap.forEach((oValue, sKey) => {
+          //   if (oValue.organizationId == this.m_oEditSubscription.organizationId) {
+          //     this.m_oOrganization = oValue;
+          //   }
+          // });
+        }
+        // this.m_bLoadingOrganizations = false;
+      },
+      error: oError => { }
+    })
   }
 }
