6f2961326786e70ad09f0979b1b9e277c8af3d02
Betty <betty.spurgeon@wasdi.cloud>
2024-04-24 11:40:29 +0200

    delete and update a user

--- src/app/components/admin/manage-users/manage-users.component.ts
+++ src/app/components/admin/manage-users/manage-users.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import FadeoutUtils from 'src/app/lib/utils/FadeoutJSUtils';
 import { AdminDashboardService } from 'src/app/services/api/admin-dashboard.service';
+import { NodeService } from 'src/app/services/api/node.service';
 import { NotificationDisplayService } from 'src/app/services/notification-display.service';
 
+const asRoles = [
+  "ADMIN",
+  "DEVELOPER",
+  "USER"
+]
 @Component({
   selector: 'app-manage-users',
   templateUrl: './manage-users.component.html',
   styleUrls: ['./manage-users.component.css']
 })
+
 export class ManageUsersComponent implements OnInit {
   m_sUserSearch: string = "";
 
@@ -29,28 +36,30 @@ export class ManageUsersComponent implements OnInit {
   //Which 
   m_iOffset: number = 0;
 
-  m_iLimit: number = 15;
+  m_iLimit: number = 10;
 
   m_bStepPageDisabled: boolean = false;
   m_bMinusPageDisabled: boolean = true;
 
+  m_aoNodes: Array<any> = [];
+
+  m_asRoles: Array<string> = asRoles
+
   constructor(private m_oAdminDashboardService: AdminDashboardService,
+    private m_oNodeService: NodeService,
     private m_oNotificationDisplayService: NotificationDisplayService) {
 
   }
 
   ngOnInit(): void {
-    // this.executeUserSearch();
     this.getUsersSummary();
     this.getPaginatedList();
-    // this.getResourceTypes();
-
+    this.getNodesList();
   }
 
   getPaginatedList() {
     this.m_oAdminDashboardService.getUsersPaginatedList("", this.m_iOffset, this.m_iLimit, null, null).subscribe({
       next: oResponse => {
-        console.log(oResponse)
         if (FadeoutUtils.utilsIsObjectNullOrUndefined(oResponse) === false) {
           this.m_aoUsers = oResponse;
         }
@@ -64,42 +73,60 @@ export class ManageUsersComponent implements OnInit {
         this.m_iTotalUsers = oResponse.totalUsers
       },
       error: oError => {
-        console.log(oError)
+        this.m_oNotificationDisplayService.openAlertDialog("Could not load user information");
       }
-    })
-  }
-
-  getResourceTypes() {
-    this.m_oAdminDashboardService.getResourceTypes().subscribe(oResponse => {
-      console.log(oResponse)
     })
   }
 
   setUserSearch(oEvent: any) {
     if (!FadeoutUtils.utilsIsStrNullOrEmpty(oEvent.event.target.value)) {
       this.m_sUserSearch = oEvent.event.target.value;
-      console.log(this.m_sUserSearch)
     }
   }
 
-  /**
-   * 
-   */
   executeUserSearch() {
     this.m_oAdminDashboardService.findUsersByPartialName(this.m_sUserSearch).subscribe({
       next: oResponse => {
         this.m_aoUsers = oResponse;
       },
-      error: oError => { }
-    })
-
-    this.m_oAdminDashboardService.getUserDetails("betty.spurgeon713@gmail.com").subscribe(oResponse => {
-      console.log(oResponse)
+      error: oError => {
+        this.m_oNotificationDisplayService.openAlertDialog('Could not find a user matching this query.')
+      }
     })
   }
-  addNewUser() { }
 
-  updateUser() { }
+  updateUser(oUser) {
+    this.m_oAdminDashboardService.updateUser(oUser).subscribe({
+      next: oResponse => {
+        this.m_oNotificationDisplayService.openSnackBar("User Updated. Refreshing.", "Close");
+        if(this.m_sUserSearch) {
+          this.executeUserSearch();
+        } else {
+          this.getPaginatedList();
+        }
+      },
+      error: oError => {
+        this.m_oNotificationDisplayService.openAlertDialog(`There was an error while trying to update ${oUser.userId}`)
+      }
+    })
+  }
+
+  deleteUser(oUser: any) {
+    console.log(oUser);
+    this.m_oNotificationDisplayService.openConfirmationDialog(`Are you sure you wish to DELETE ${oUser.userId}? <br> This is a destructive action and cannot be undone`).subscribe(oResponse => {
+      if (oResponse === true) {
+        this.m_oAdminDashboardService.deleteUser(oUser.userId).subscribe({
+          next: oResponse => {
+            this.getUsersSummary();
+            this.getPaginatedList();
+          },
+          error: oError => {
+            this.m_oNotificationDisplayService.openAlertDialog(`An error occured while trying to delete ${oUser.userId}`)
+          }
+        })
+      }
+    })
+  }
 
   setSelectedUser(oUser) {
     if (!FadeoutUtils.utilsIsObjectNullOrUndefined(oUser)) {
@@ -109,6 +136,7 @@ export class ManageUsersComponent implements OnInit {
             this.m_oNotificationDisplayService.openAlertDialog("Could not load user information")
           } else {
             this.m_oSelectedUser = oResponse
+            console.log(this.m_oSelectedUser);
           }
         },
         error: oError => { }
@@ -116,11 +144,32 @@ export class ManageUsersComponent implements OnInit {
     }
   }
 
+  getNodesList() {
+    this.m_oNodeService.getNodesList().subscribe({
+      next: oResponse => {
+        if (FadeoutUtils.utilsIsObjectNullOrUndefined(oResponse)) {
+          this.m_oNotificationDisplayService.openAlertDialog("Error while retrieving the nodes list");
+        } else {
+          this.m_aoNodes = oResponse.map(oNode => {
+            return oNode.nodeCode
+          });
+
+
+        }
+      },
+      error: oError => {
+        this.m_oNotificationDisplayService.openAlertDialog("Could not get the Nodes list.")
+      }
+    })
+  }
+
+  /********** Pagination Handlers **********/
   stepOnePage() {
     this.m_iOffset += this.m_iLimit;
     if (this.m_iOffset >= this.m_iTotalUsers) {
       this.m_bStepPageDisabled = true;
     } else {
+      this.m_bStepPageDisabled = false
       this.getPaginatedList();
       this.m_bMinusPageDisabled = false;
     }
@@ -128,9 +177,34 @@ export class ManageUsersComponent implements OnInit {
 
   minusOnePage() {
     this.m_iOffset -= this.m_iLimit;
+    this.m_bStepPageDisabled = false;
     this.getPaginatedList();
     if (this.m_iOffset <= 0) {
       this.m_bMinusPageDisabled = true;
     }
   }
+
+  setInputSelections(oEvent: any, sLabel: string) {
+    switch (sLabel) {
+      case 'isActive':
+        this.m_oSelectedUser.active = oEvent.target.checked;
+        break;
+      case 'userId':
+        this.m_oSelectedUser.userId = oEvent.event.target.value;
+        break;
+      case 'name':
+        this.m_oSelectedUser.name = oEvent.event.target.value;
+        break;
+      case 'surname':
+        this.m_oSelectedUser.surname = oEvent.event.target.value;
+        break;
+      case 'node':
+        this.m_oSelectedUser.defaultNode = oEvent.event.target.value;
+        break;
+      default:
+        break;
+    }
+  }
+
+
 }
