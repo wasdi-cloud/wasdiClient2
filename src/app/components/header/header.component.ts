075b6eca5ccb9106e97e416849ca8acf7483800c
Betty <betty.spurgeon@wasdi.cloud>
2024-05-02 09:31:08 +0200

    Feedback form and clean code from header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -24,15 +24,9 @@ export class HeaderComponent implements OnInit {
   m_oProject: any;
   m_oSelectedProject: any = { name: "No Active Project", projectId: null };
 
-  m_oFeedback: {
-    title: string | null,
-    message: string | null
-  } = { title: null, message: null }
-
   constructor(
     private m_oConstantsService: ConstantsService,
     private m_oDialog: MatDialog,
-    private m_oFeedbackService: FeedbackService,
     private m_oNotificationDisplayService: NotificationDisplayService,
     private m_oProjectService: ProjectService,
     private m_oRouter: Router
@@ -137,23 +131,6 @@ export class HeaderComponent implements OnInit {
 
   openDocs() {
     window.open('https://discord.gg/FkRu2GypSg', '_blank')
-  }
-
-  sendFeedback() {
-    if (FadeoutUtils.utilsIsObjectNullOrUndefined(this.m_oFeedback) || !this.m_oFeedback.title || !this.m_oFeedback.message) {
-      console.log("Error sending message");
-      return false;
-    }
-    this.m_oFeedbackService.sendFeedback(this.m_oFeedback).subscribe(oResponse => {
-      if (!FadeoutUtils.utilsIsObjectNullOrUndefined(oResponse) && oResponse.boolValue === true) {
-        console.log("feedback sent")
-        return true;
-      } else {
-        console.log("error sending feedback");
-        return false;
-      }
-    });
-    return true;
   }
 }
 
