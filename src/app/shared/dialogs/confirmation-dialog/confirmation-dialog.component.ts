c8f604c9284cb560c4fa0a93928090e73060838b
Betty <betty.spurgeon@wasdi.cloud>
2024-05-28 08:39:54 +0200

    confirmation/alert dialog combined

--- src/app/shared/dialogs/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/shared/dialogs/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -6,25 +6,33 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   templateUrl: './confirmation-dialog.component.html',
   styleUrls: ['./confirmation-dialog.component.css']
 })
-export class ConfirmationDialogComponent {
+export class ConfirmationDialogComponent implements OnInit {
   m_sTitle: string;
   m_sMessage: string;
+  m_sClassName: 'generic' | 'success' | 'alert' | 'danger' | 'info' | string = "generic";
+  m_bIsConfirmation: boolean = false;
 
   constructor(
     public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ConfirmationDialogModel) {
-    this.m_sTitle = data.title;
-    this.m_sMessage = data.message;
+
+  }
+  ngOnInit(): void {
+    this.m_sTitle = this.data.title;
+    this.m_sMessage = this.data.message;
+    this.m_sClassName = this.data.className
+    this.m_bIsConfirmation = this.data.isConfirmation
   }
 
   onConfirm(): void {
     this.dialogRef.close(true)
   }
+
   onDismiss(): void {
     this.dialogRef.close(false);
   }
 }
 
 export class ConfirmationDialogModel {
-  constructor(public title: string, public message: string) { }
+  constructor(public title: string, public message: string, public className: string, public isConfirmation: boolean) { }
 }
