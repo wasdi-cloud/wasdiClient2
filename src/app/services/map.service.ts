ad38c5017f394f92b190e5211d5acb3df809a72d
Betty <betty.spurgeon@wasdi.cloud>
2024-05-30 17:51:55 +0200

    Missing map marker - fixed

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -12,6 +12,22 @@ import 'node_modules/leaflet-mouse-position/src/L.Control.MousePosition.js'
 
 declare const L: any;
 
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 @Injectable({
   providedIn: 'root'
 })
