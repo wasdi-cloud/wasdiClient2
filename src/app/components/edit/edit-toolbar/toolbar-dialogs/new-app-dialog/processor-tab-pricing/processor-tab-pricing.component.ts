46542c159a06cc817c6765160e378519dc82ac88
Betty <betty.spurgeon@wasdi.cloud>
2024-06-06 09:22:15 +0200

    Is Free flag for pricing tab

--- src/app/components/edit/edit-toolbar/toolbar-dialogs/new-app-dialog/processor-tab-pricing/processor-tab-pricing.component.ts
+++ src/app/components/edit/edit-toolbar/toolbar-dialogs/new-app-dialog/processor-tab-pricing/processor-tab-pricing.component.ts
@@ -6,35 +6,68 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./processor-tab-pricing.component.css']
 })
 export class ProcessorTabPricingComponent implements OnInit {
+  /**
+   * Form input for the store information
+   */
   @Input() m_oProcessorStoreInfo
 
+  /**
+   * Flag to track if the app is free
+   */
   m_bIsFree: boolean = true;
+
+  /**
+   * Intended price
+   */
+  m_iOnDemandPrice: number = 0;
+
+  /**
+   * Placeholder for original price (used when toggling free/paid)
+   */
+  m_iOriginalPrice: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
     if (this.m_oProcessorStoreInfo) {
       if (this.m_oProcessorStoreInfo.value) {
-        if (this.m_oProcessorStoreInfo.value.iOnDemandPrice == 0) this.m_bIsFree =true;
-        else this.m_bIsFree = false;
+        if (this.m_oProcessorStoreInfo.value.iOnDemandPrice == 0) {
+          this.m_bIsFree = true;
+        } else {
+          this.m_bIsFree = false;
+          this.m_iOnDemandPrice = this.m_oProcessorStoreInfo.value.iOnDemandPrice;
+          this.m_iOriginalPrice = this.m_oProcessorStoreInfo.value.iOnDemandPrice;
+        }
       }
     }
   }
 
   patchShowInStore(oEvent) {
-    console.log(oEvent.target.checked);
     this.m_oProcessorStoreInfo.patchValue({
       bShowInStore: oEvent.target.checked
     })
   }
 
   patchOnDemandPrice(oEvent) {
-    console.log(oEvent.event.target.value)
+    this.m_bIsFree = oEvent.event.target.value === 0;
 
-    this.m_bIsFree = oEvent.event.target.value == 0;
+    this.m_iOriginalPrice = oEvent.event.target.value;
 
     this.m_oProcessorStoreInfo.patchValue({
       iOnDemandPrice: oEvent.event.target.value
     })
+  }
+
+  getFreeStatus(oEvent) {
+    this.m_bIsFree = oEvent.target.checked
+
+    if (this.m_bIsFree === true) {
+      this.m_iOnDemandPrice = 0;
+      this.m_oProcessorStoreInfo.patchValue({
+        iOnDemandPrice: 0
+      })
+    } else {
+      this.m_iOnDemandPrice = this.m_iOriginalPrice
+    }
   }
 }
